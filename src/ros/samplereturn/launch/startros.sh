e52a39ea434dbf791d10632426949d1818e4b0ad
contradict <contradict@gmail.com>
2012-04-15 14:24:40 -0700

    Split logging files and autostart
    startros.sh should be called by an autostart file on user login

---
+++ src/ros/samplereturn/launch/startros.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. /opt/ros/electric/setup.bash
+export ROS_PACKAGE_PATH=/home/robot/Desktop/SampleReturn/src/ros:$ROS_PACKAGE_PATH
+roscore &
+
+sleep 5
+
+roslaunch samplereturn logging.launch
