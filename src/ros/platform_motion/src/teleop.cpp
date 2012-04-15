8509cc9bf9a46b0c9f0b8f9ecb525f1f4fcdc1f0
contradict <contradict@gmail.com>
2012-04-15 14:13:16 -0700

    Add exponential joystick scaling

--- src/ros/platform_motion/src/teleop.cpp
+++ src/ros/platform_motion/src/teleop.cpp
@@ -12,13 +12,15 @@ public:
   Teleop();
 
 private:
+  double scale_joystick(double scale, double exponent, double value);
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
   void doHoming(void);
   
   ros::NodeHandle nh_;
 
   int linear_x, linear_y, angular_z, button_homing;
-  double l_scale_, a_scale_;
+  double l_scale_, l_exp_;
+  double a_scale_, a_exp_;
   ros::Publisher twist_pub_;
   ros::Subscriber joy_sub_;
   bool homing;
@@ -33,20 +35,24 @@ Teleop::Teleop():
   angular_z(2),
   button_homing(1),
   l_scale_(2),
+  l_exp_(2),
   a_scale_(M_PI),
+  a_exp_(2),
   homing(false),
   ac("home_wheelpods")
 {
-  nh_.param("axis_linear_x", linear_x, linear_x);
-  nh_.param("axis_linear_y", linear_y, linear_y);
-  nh_.param("axis_angular_z", angular_z, angular_z);
-  nh_.param("button_homing", button_homing, button_homing);
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
+    nh_.param("axis_linear_x", linear_x, linear_x);
+    nh_.param("axis_linear_y", linear_y, linear_y);
+    nh_.param("axis_angular_z", angular_z, angular_z);
+    nh_.param("button_homing", button_homing, button_homing);
+    nh_.param("scale_angular", a_scale_, a_scale_);
+    nh_.param("exponent_angular", a_exp_, a_exp_);
+    nh_.param("scale_linear", l_scale_, l_scale_);
+    nh_.param("exponent_linear", l_exp_, l_exp_);
 
-  twist_pub_ = nh_.advertise<geometry_msgs::Twist>("twist", 1);
+    twist_pub_ = nh_.advertise<geometry_msgs::Twist>("twist", 1);
 
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &Teleop::joyCallback, this);
+    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &Teleop::joyCallback, this);
 
 }
 
@@ -70,6 +76,12 @@ void Teleop::doHoming(void)
     homing=false;
 }
 
+
+double Teleop::scale_joystick(double scale, double exponent, double value)
+{
+    return copysign(scale*pow(fabs(value), exponent), value);
+}
+
 void Teleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
     if(homing) {
@@ -79,9 +91,9 @@ void Teleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
             boost::thread(boost::bind(&Teleop::doHoming, this));
         } else {
             geometry_msgs::Twist twist;
-            twist.angular.z = a_scale_*joy->axes[angular_z];
-            twist.linear.x = l_scale_*joy->axes[linear_x];
-            twist.linear.y = l_scale_*joy->axes[linear_y];
+            twist.angular.z = scale_joystick(a_scale_, a_exp_, joy->axes[angular_z]);
+            twist.linear.x = scale_joystick(l_scale_, l_exp_, joy->axes[linear_x]);
+            twist.linear.y = scale_joystick(l_scale_, l_exp_, joy->axes[linear_y]);
             twist_pub_.publish(twist);
         }
     }
