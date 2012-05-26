f4bbeebf5df4a903e7c8c75bbce490a2cb90168d
contradict <contradict@gmail.com>
2012-05-26 13:57:32 -0700

    Begin multi-machine split

--- src/ros/samplereturn/launch/startros.sh
+++ src/ros/samplereturn/launch/startros.sh
@@ -2,10 +2,11 @@
 
 . /opt/ros/electric/setup.bash
 export ROS_PACKAGE_PATH=/home/robot/Desktop/SampleReturn/src/ros:$ROS_PACKAGE_PATH
-roscore &
 
-sleep 5
-
-roslaunch samplereturn driving_test.launch &
-#roslaunch samplereturn logging.launch &
+if [ `hostname` = "BLUE18" ]; then
+	roscore &
+	sleep 5
+	roslaunch samplereturn driving_test.launch &
+	# roslaunch samplereturn logging.launch &
+fi
 
