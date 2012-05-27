51b61f228112df5202df7297ce486272bcff2b28
contradict <contradict@gmail.com>
2012-05-26 20:18:08 -0700

    More robust startros.sh
    fix blue18 hostname
    add wakeonlan commands
    check to be sure other machine is up before launching
    Turn off display
    use overlay setup file
    check for running roscore before starting it
    emit launch pid files
    launch files don't work if they are run in immediate sequence,
    slow everything down a bit.

--- src/ros/samplereturn/launch/startros.sh
+++ src/ros/samplereturn/launch/startros.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-. /opt/ros/electric/setup.bash
-export ROS_PACKAGE_PATH=/home/robot/Desktop/SampleReturn/src/ros:$ROS_PACKAGE_PATH
+xset dpms force off
 
-if [ `hostname` = "BLUE18" ]; then
-	roscore &
-	sleep 5
-	roslaunch samplereturn driving_test.launch &
-	# roslaunch samplereturn logging.launch &
+. /opt/ros/opencv_overlay/overlay_setup.sh
+export ROSLAUNCH_SSH_UNKNOWN=1
+export ROS_PACKAGE_PATH="/home/robot/Desktop/SampleReturn/src/ros:$ROS_PACKAGE_PATH"
+export ROS_MASTER_URI="http://blue18:11311"
+
+echo "ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}"
+
+sleep 10
+
+if [ `hostname` = "blue18" ]; then
+    if ! pidof python | grep `cat ${HOME}/.ros/roscore-11311.pid 2>/dev/null` >/dev/null 2>&1; then
+        roscore &
+    fi
+    sleep 10
+    until ping -c1 -w5 moline >>/dev/null 2>&1; do
+        wakeonlan moline
+        sleep 5
+    done
+	roslaunch --pid=${HOME}/.ros/driving_test.pid samplereturn driving_test.launch &
+    # sleep 10
+	# roslaunch --pid=${HOME}/.ros/logging.pid samplereturn logging.launch &
+else
+    until ping -c1 -w5 blue18 >>/dev/null 2>&1; do
+        wakeonlan blue18
+        sleep 5
+    done
 fi
 
