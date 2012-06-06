c715be8f82c40d7a4ac3e560655be2e18f319567
contradict <contradict@gmail.com>
2012-06-06 09:06:58 -0700

    Tweak test node
    switch to button 5
    make subscriptions/clients global
    typos

--- src/ros/manipulator/src/manipulator_test_node.py
+++ src/ros/manipulator/src/manipulator_test_node.py
@@ -8,26 +8,26 @@ from manipulator.msg import ManipulatorGrabAction, ManipulatorGrabGoal, Manipula
 
 from sensor_msgs.msg import Joy
 
+buttonGrab = rospy.get_param('button_grab', 5)
+
+manipulatorClient = actionlib.SimpleActionClient(
+  'manipulator_grab',
+  ManipulatorGrabAction
+)
+
 def JoystickCallback(msg):
   # when the right button is pushed, emit a manipulator grab command!
-  if msg.buttons[self.buttonGrab] == 1:
+  if msg.buttons[buttonGrab] == 1:
     goal = ManipulatorGrabGoal()
-    goal.wrist_angle = 0.0
-    self.manipulatorClient.send(goal)
-    self.manipulatorClient.wait_for_result()
+    goal.wrist_angle = 0.5
+    manipulatorClient.send_goal(goal)
+    manipulatorClient.wait_for_result()
 
 def main():
   rospy.init_node("manipulator_test_node")
 
-  self.buttonGrab = rospy.param('button_grab', 4)
-
-  self.manipulatorClient = actionlib.SimpleActionClient(
-      'manipulator_grab',
-      ManipulatorGrabAction
-  )
-
   # wait for the server to actualy show up before doing anything
-  self.manipulatorClient.wait_for_server()
+  manipulatorClient.wait_for_server()
 
   joystickSubscriber = rospy.Subscriber('joy', Joy, JoystickCallback)
 
