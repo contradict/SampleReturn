9e402b6b9ced2a2007028ec0cc95af30811c4582
contradict <contradict@gmail.com>
2013-01-27 14:32:01 -0800

    Proper installation of manipulator nodes

--- ros_workspace/src/manipulator/setup.py
+++ ros_workspace/src/manipulator/setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-           scripts=['src/manipulator_node.py',
-                    'src/manipulator_test_node.py'],
+           scripts=['nodes/manipulator_node.py',
+                    'nodes/manipulator_test_node.py'],
            packages=['manipulator'],
-           package_dir={'manipulator': 'src'}
+           package_dir={'manipulator': 'src/manipulator'}
           )
 
 setup(**d)
