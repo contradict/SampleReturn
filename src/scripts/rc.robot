b0cb2e6abda0371609b516c3bf6fb4dbe965a856
contradict <contradict@gmail.com>
2013-02-09 13:37:31 -0800

    Update startup script
    This one seems to actually start the robot on powerup

--- src/scripts/rc.robot
+++ src/scripts/rc.robot
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if /sbin/start-stop-daemon \ 
-    --start \ 
-    --user robot \ 
-    --chuid robot \ 
-    --pidfile ~robot/.ros/roscore-11311.pid \ 
-    /home/robot/Desktop/SampleReturn/src/scripts/startup.sh; then 
-true 
-else 
-    /usr/bin/aplay /usr/lib/libreoffice/share/gallery/sounds/strom.wav & 
-fi 
+/sbin/start-stop-daemon --user robot --chuid robot --pidfile /home/robot/.ros/roscore-11311.pid --exec /home/robot/Desktop/SampleReturn/src/scripts/startup.sh --start
+
+if [ "$?" != "0" ]; then
+    /usr/bin/aplay /usr/lib/libreoffice/share/gallery/sounds/strom.wav &
+fi
 
--- src/scripts/startup.sh
+++ src/scripts/startup.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-MASTER_HOST="sr1-internal"
+MASTER_HOST="sr1"
 PATH=/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+HOME=/home/robot
+if [ $# -eq 0 ]; then
+    LAUNCH_FILE=driving_test.launch
+else
+    LAUNCH_FILE=$1
+fi
 
-cd ~robot/Desktop/SampleReturn
+eval `ssh-agent`
+ssh-add ${HOME}/.ssh/robot_rsa
+
+cd ${HOME}/Desktop/SampleReturn
 . /opt/ros/groovy/setup.bash
 . ros_workspace/install/setup.bash
 
 export ROSLAUNCH_SSH_UNKNOWN=1
 export ROS_MASTER_URI="http://${MASTER_HOST}:11311"
 
-if echo ${MASTER_HOST} | grep -q sr1; then
+if echo ${MASTER_HOST} | grep -q `hostname`; then
 
     if ! pidof python | grep `cat ${HOME}/.ros/roscore-11311.pid 2>/dev/null` >/dev/null 2>&1; then
+        echo "Starting new core process"
         roscore &
     fi
 
-    #sleep 10
     /usr/bin/aplay /usr/lib/libreoffice/share/gallery/sounds/romans.wav
-
-    roslaunch --pid=${HOME}/.ros/driving_test.pid samplereturn driving_test.launch &
+    pidname=`basename ${LAUNCH_FILE} .launch`.pid
+    roslaunch --pid=${HOME}/.ros/${pidname} samplereturn ${LAUNCH_FILE} &
 fi
-
