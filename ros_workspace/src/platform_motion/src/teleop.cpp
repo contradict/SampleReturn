edf699e54a5c66c0d7954b9cffa90d8ee42705b9
contradict <contradict@gmail.com>
2013-03-17 16:38:50 -0700

    Remove enable/disable from teleop node
    These are handled by pause_switch now.

--- ros_workspace/src/platform_motion/src/teleop.cpp
+++ ros_workspace/src/platform_motion/src/teleop.cpp
@@ -16,7 +16,6 @@ private:
   double scale_joystick(double scale, double exponent, double value);
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
   void doHoming(void);
-  void doEnable(bool state);
   void timerCallback(const ros::TimerEvent &);
   
   ros::NodeHandle nh_;
@@ -24,7 +23,7 @@ private:
   ros::Timer pub_timer;
 
   int linear_x, linear_y, angular_z;
-  int button_homing, button_enable, button_disable;
+  int button_homing;
   double l_scale_, l_exp_;
   double a_scale_, a_exp_;
   ros::Publisher twist_pub_;
@@ -41,8 +40,6 @@ Teleop::Teleop():
   linear_y(0),
   angular_z(2),
   button_homing(1),
-  button_enable(2),
-  button_disable(3),
   l_scale_(2),
   l_exp_(2),
   a_scale_(M_PI),
@@ -58,8 +55,6 @@ Teleop::Teleop():
     nh_.param("/teleop/axis_linear_y", linear_y, linear_y);
     nh_.param("/teleop/axis_angular_z", angular_z, angular_z);
     nh_.param("/teleop/button_homing", button_homing, button_homing);
-    nh_.param("/teleop/button_enable", button_enable, button_enable);
-    nh_.param("/teleop/button_disable", button_disable, button_disable);
     nh_.param("/teleop/scale_angular", a_scale_, a_scale_);
     nh_.param("/teleop/exponent_angular", a_exp_, a_exp_);
     nh_.param("/teleop/scale_linear", l_scale_, l_scale_);
@@ -95,18 +90,6 @@ void Teleop::doHoming(void)
     homing=false;
 }
 
-void Teleop::doEnable(bool state)
-{
-    platform_motion::Enable enmsg;
-    enmsg.request.state=state;
-    ROS_INFO("calling enable pods: %s ", state?"enable":"disable");
-    if(ros::service::call("/enable_wheel_pods", enmsg)) {
-        ROS_INFO("wheel pods %s complete", state?"enable":"disable");
-    } else {
-        ROS_ERROR("enable wheel pods failed");
-    }
-}
-
 double Teleop::scale_joystick(double scale, double exponent, double value)
 {
     return copysign(scale*pow(fabs(value), exponent), value);
@@ -118,20 +101,9 @@ void Teleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     {
         boost::thread(boost::bind(&Teleop::doHoming, this));
     }
-    else if(joy->buttons[button_enable])
-    {
-        boost::thread(boost::bind(&Teleop::doEnable, this, _1), true);
-    }
-    else if(joy->buttons[button_disable])
-    {
-        boost::thread(boost::bind(&Teleop::doEnable, this, _1), false);
-    }
-    else
-    {
-        vel_theta=scale_joystick(a_scale_, a_exp_, joy->axes[angular_z]);
-        vel_x=scale_joystick(l_scale_, l_exp_, joy->axes[linear_x]);
-        vel_y=scale_joystick(l_scale_, l_exp_, joy->axes[linear_y]);
-    }
+    vel_theta=scale_joystick(a_scale_, a_exp_, joy->axes[angular_z]);
+    vel_x=scale_joystick(l_scale_, l_exp_, joy->axes[linear_x]);
+    vel_y=scale_joystick(l_scale_, l_exp_, joy->axes[linear_y]);
 }
 
 void Teleop::timerCallback(const ros::TimerEvent &evt)
