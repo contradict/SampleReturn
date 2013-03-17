3df76c09450674a2e497960540d6c64c8fd67aca
contradict <contradict@gmail.com>
2013-03-16 17:38:32 -0700

    image_desync now explicitly notices camera failures
    nan for both, -inf for left, inf for right

--- ros_workspace/src/samplereturn/nodes/image_desync.py
+++ ros_workspace/src/samplereturn/nodes/image_desync.py
@@ -14,8 +14,17 @@ class image_desync(object):
         self.right_sub = rospy.Subscriber('right/camera_info', CameraInfo,
                 lambda info, name='right': self.info_callback(name, info))
 
+        self.check_interval = rospy.get_param("check_interval", 1.0)
+        self.timer = rospy.Timer(rospy.Duration(self.check_interval),
+                self.check_for_data)
+        # not really, but this gives a check_interval
+        # pause before the first error message
+        self.got_info={'left':True,
+                       'right':True
+                       }
 
     def info_callback(self, name, info):
+        self.got_info[name] = True
         self.timestamps[name].append(info.header.stamp)
         if len(self.timestamps[name]) >2:
            self.timestamps[name].pop(0)
@@ -29,6 +38,18 @@ class image_desync(object):
             else:
                 delta = delta2
             self.pub.publish(Float64(delta))
+            self.timestamps['left'].pop(0)
+            self.timestamps['right'].pop(0)
+
+    def check_for_data(self, event):
+        if not (self.got_info['left'] or self.got_info['right']):
+            self.pub.publish(Float64(float('nan')))
+        elif not self.got_info['left']:
+            self.pub.publish(Float64(-float('inf')))
+        elif not self.got_info['right']:
+            self.pub.publish(Float64(float('inf')))
+        self.got_info['left'] = False
+        self.got_info['right'] = False
 
 if __name__=="__main__":
     rospy.init_node('image_desync')
