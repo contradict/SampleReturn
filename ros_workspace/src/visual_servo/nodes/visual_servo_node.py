b4d2ed769c78b35f651604ed8d4e9ea1512ef1a2
Brandon <brandon.m.booth@gmail.com>
2013-04-10 00:00:11 -0700

    Flipped rotation signs in visual servo code and cleaned it up a tiny bit

--- ros_workspace/src/visual_servo/nodes/visual_servo_node.py
+++ ros_workspace/src/visual_servo/nodes/visual_servo_node.py
@@ -16,7 +16,7 @@ class VisualServo:
 	
 	def __init__(self):
 		rospy.init_node('visual_servo')
-		rospy.Subscriber('/sample_detection/red_puck_imgpoints', Point, self.object_point_callback)
+		rospy.Subscriber('/red_puck_imgpoints', Point, self.object_point_callback)
 		rospy.Subscriber('/navigation/left/camera_info', CameraInfo, self.camera_left_info_callback)
 		rospy.Subscriber('/navigation/right/camera_info', CameraInfo, self.camera_right_info_callback)
 		self._publisher = rospy.Publisher('/servo_command', Twist)
@@ -24,12 +24,16 @@ class VisualServo:
 		self._camera_left_height = -1
 		self._camera_right_width = -1
 		self._camera_right_height = -1
-		self._camera_left_width_window = 1
-		self._camera_left_height_window = 1
-		self._camera_right_width_window = 1
-		self._camera_right_height_window = 1
-		self._camera_left_target_height = 400
-		self._camera_right_target_height = 400
+		self._camera_left_width_window = 3
+		self._camera_left_height_window = 5
+		self._camera_right_width_window = 3
+		self._camera_right_height_window = 5
+		self._camera_left_target_width = 330
+		self._camera_right_target_width = 150
+		self._camera_left_target_height = 256
+		self._camera_right_target_height = 256
+		self._rotation_velocity = 0.1
+		self._linear_velocity = 0.1
 		self._is_trying_to_work = True
 
 	def object_point_callback(self, data):
@@ -46,27 +50,27 @@ class VisualServo:
 				twist.angular.z = 0.0
 				twist.angular.y = 0.0
 				twist.angular.x = 0.0
-				twist.linear.x = 0.0
-				twist.linear.y = 0.02
+				twist.linear.x = self._linear_velocity
+				twist.linear.y = 0.0
 				twist.linear.z = 0.0
 			# Else if the object is near the bottom of the image, move backwards
 			if data.y > self._camera_left_height*0.9:
 				twist.angular.z = 0.0
 				twist.angular.y = 0.0
 				twist.angular.x = 0.0
-				twist.linear.x = 0.0
-				twist.linear.y = -0.02
+				twist.linear.x = -self._linear_velocity
+				twist.linear.y = 0.0
 				twist.linear.z = 0.0
-			# Else if the object is not centered along the horizontal axis, rotate until it is
-			elif data.x > (self._camera_left_width/2.0)+self._camera_left_width_window:
-				twist.angular.z = 0.01
+			# Else if the object is not at the target width, rotate until it is
+			elif data.x > self._camera_left_target_width+self._camera_left_width_window:
+				twist.angular.z = -self._rotation_velocity
 				twist.angular.y = 0.0
 				twist.angular.x = 0.0
 				twist.linear.x = 0.0
 				twist.linear.y = 0.0
 				twist.linear.z = 0.0
-			elif data.x < (self._camera_left_width/2.0)-self._camera_left_width_window:
-				twist.angular.z = -0.01
+			elif data.x < self._camera_left_target_width-self._camera_left_width_window:
+				twist.angular.z = self._rotation_velocity
 				twist.angular.y = 0.0
 				twist.angular.x = 0.0
 				twist.linear.x = 0.0
@@ -77,15 +81,15 @@ class VisualServo:
 				twist.angular.z = 0.0
 				twist.angular.y = 0.0
 				twist.angular.x = 0.0
-				twist.linear.x = 0.0
-				twist.linear.y = 0.02
+				twist.linear.x = self._linear_velocity
+				twist.linear.y = 0.0
 				twist.linear.z = 0.0
 			elif data.y > (self._camera_left_target_height+self._camera_left_height_window):
 				twist.angular.z = 0.0
 				twist.angular.y = 0.0
 				twist.angular.x = 0.0
-				twist.linear.x = 0.0
-				twist.linear.y = -0.02
+				twist.linear.x = -self._linear_velocity
+				twist.linear.y = 0.0
 				twist.linear.z = 0.0
 			# Else the object should be aligned at the proper location in the image
 			else:
@@ -112,13 +116,21 @@ class VisualServo:
 		self._camera_right_height_window = max(1, data.height*0.01)
 
 	def do_visual_servo(self):
-		self._sleep_duration = rospy.Duration(10)
+		self._sleep_duration = rospy.Duration(2)
 		while not rospy.is_shutdown():
 			self._is_trying_to_work = False
 			rospy.sleep(self._sleep_duration)
 			if not self._is_trying_to_work:
 				# TODO - Let the executive ROS node know that this node has failed to do anything useful for a while
-				rospy.logerr("Failed to do any work for ten seconds!")
+				twist = Twist()
+				twist.angular.z = 0.0
+				twist.angular.y = 0.0
+				twist.angular.x = 0.0
+				twist.linear.x = 0.0
+				twist.linear.y = 0.0
+				twist.linear.z = 0.0
+				self._publisher.publish(twist)
+				rospy.logerr("Failed to do any work for two seconds!")
 
 if __name__ == '__main__':
 	visual_servo = VisualServo()
