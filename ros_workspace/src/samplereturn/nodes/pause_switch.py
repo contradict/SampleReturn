4a8e419be94f0967f675dfdd60d3867e50ff5a09
Jascha <jtlittle@gmail.com>
2013-04-24 18:23:57 -0700

    Wait for all pause services before reading GPIO.

--- ros_workspace/src/samplereturn/nodes/pause_switch.py
+++ ros_workspace/src/samplereturn/nodes/pause_switch.py
@@ -29,15 +29,15 @@ class PauseSwitch(object):
         self.enable_wheelpods = rospy.ServiceProxy('/enable_wheel_pods', Enable)
         self.enable_carousel = rospy.ServiceProxy('/enable_carousel', Enable)
 
+        rospy.loginfo('Pause_switch waiting for manipulator/pause service...')
+        rospy.wait_for_service('/manipulator/pause')
+        self.manipulator_pause_service = rospy.ServiceProxy('/manipulator/pause', Enable)
+
         rospy.Subscriber("/gpio_read", GPIO, self.gpio)
         rospy.Subscriber("/status_word", ServoStatus, self.status_word)
 
         self.pause_pub = rospy.Publisher("pause_state", Bool)
         self.audio_pub = rospy.Publisher("/audio/navigate", SoundRequest)
-
-        rospy.loginfo('Pause_switch waiting for manipulator/pause service...')
-        rospy.wait_for_service('/manipulator/pause')
-        self.manipulator_pause_service = rospy.ServiceProxy('/manipulator/pause', Enable)
 
         self.paused = None
 
