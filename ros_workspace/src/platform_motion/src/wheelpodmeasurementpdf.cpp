9a0b7556d8ba4b37739c5b23389c1d0f6410a85b
contradict <contradict@gmail.com>
2013-04-24 22:47:32 -0700

    Add nonlinear system model to odometry EKF
    Now odomtery integation occurs inside the filter so meaningful covariances come
    out.

--- ros_workspace/src/platform_motion/src/wheelpodmeasurementpdf.cpp
+++ ros_workspace/src/platform_motion/src/wheelpodmeasurementpdf.cpp
@@ -36,45 +36,45 @@ MatrixWrapper::Matrix
 WheelPodMeasurementPdf::dfGet(unsigned int i) const
 {
     MatrixWrapper::Matrix pod_direction = computePodDirections();
-    MatrixWrapper::Matrix df(6,3);
+    MatrixWrapper::Matrix df(6,6);
 
     if(i==0)
     {
-        //[[ dv0/dvx         dv0/dvy         dv0/domega]
-        // [ dv0_perp/dvx    dv0_perp/dvy    dv0_perp/domega]
-        // [ dv1/dvx         dv1/dvy         dv1/domega]
-        // [ dv1_perp/dvx    dv1_perp/dvy    dv1_perp/domega]
-        // [ dv2/dvx         dv2/dvy         dv2/domega]]
-        // [ dv2_perp/dvx    dv2_perp/dvy    dv2_perp/domega]]
-
-        df(1,1) =      pod_direction(1,1);
-        df(1,2) =      pod_direction(2,1);
-        df(1,3) = -1.0*pod_positions(2,1)*pod_direction(1,1)
+        //[[ dv0/dx       dv0/dy       dv0/dtheta       dv0/dvx         dv0/dvy         dv0/domega]
+        // [ dv0_perp/dx  dv0_perp/dy  dv0_perp/dtheta  dv0_perp/dvx    dv0_perp/dvy    dv0_perp/domega]
+        // [ dv1/dx       dv1/dy       dv1/dtheta       dv1/dvx         dv1/dvy         dv1/domega]
+        // [ dv1_perp_dx  dv1_perp_dy  dv1_perp_dtheta  dv1_perp/dvx    dv1_perp/dvy    dv1_perp/domega]
+        // [ dv2/dx       dv2/dy       dv2/dtheta       dv2/dvx         dv2/dvy         dv2/domega]]
+        // [ dv2_perp/dx  dv2_perp/dy  dv2_perp/dtheta  dv2_perp/dvx    dv2_perp/dvy    dv2_perp/domega]]
+        df      = 0;
+        df(1,4) =      pod_direction(1,1);
+        df(1,5) =      pod_direction(2,1);
+        df(1,6) = -1.0*pod_positions(2,1)*pod_direction(1,1)
                   +    pod_positions(1,1)*pod_direction(2,1);
 
-        df(2,1) = -1.0*pod_direction(2,1); 
-        df(2,2) =      pod_direction(1,1); 
-        df(2,3) =      pod_positions(2,1)*pod_direction(2,1)
+        df(2,4) = -1.0*pod_direction(2,1); 
+        df(2,5) =      pod_direction(1,1); 
+        df(2,6) =      pod_positions(2,1)*pod_direction(2,1)
                   +    pod_positions(1,1)*pod_direction(1,1);
 
-        df(3,1) =      pod_direction(1,2);
-        df(3,2) =      pod_direction(2,2);
-        df(3,3) = -1.0*pod_positions(2,2)*pod_direction(1,2)
+        df(3,4) =      pod_direction(1,2);
+        df(3,5) =      pod_direction(2,2);
+        df(3,6) = -1.0*pod_positions(2,2)*pod_direction(1,2)
                   +    pod_positions(1,2)*pod_direction(2,2);
 
-        df(4,1) = -1.0*pod_direction(2,2); 
-        df(4,2) =      pod_direction(1,2); 
-        df(4,3) =      pod_positions(2,2)*pod_direction(2,2)
+        df(4,4) = -1.0*pod_direction(2,2); 
+        df(4,5) =      pod_direction(1,2); 
+        df(4,6) =      pod_positions(2,2)*pod_direction(2,2)
                   +    pod_positions(1,2)*pod_direction(1,2);
 
-        df(5,1) = pod_direction(1,3);
-        df(5,2) = pod_direction(2,3);
-        df(5,3) = -1*pod_positions(2,3)*pod_direction(1,3)
+        df(5,4) = pod_direction(1,3);
+        df(5,5) = pod_direction(2,3);
+        df(5,6) = -1*pod_positions(2,3)*pod_direction(1,3)
                   +  pod_positions(1,3)*pod_direction(2,3);
 
-        df(6,1) = -1.0*pod_direction(2,3); 
-        df(6,2) =      pod_direction(1,3); 
-        df(6,3) =      pod_positions(2,3)*pod_direction(2,3)
+        df(6,4) = -1.0*pod_direction(2,3); 
+        df(6,5) =      pod_direction(1,3); 
+        df(6,6) =      pod_positions(2,3)*pod_direction(2,3)
                   +    pod_positions(1,3)*pod_direction(1,3);
     }
     else
@@ -92,8 +92,8 @@ WheelPodMeasurementPdf::ExpectedValueGet(void) const
     MatrixWrapper::ColumnVector z(6);
 
     MatrixWrapper::ColumnVector vel(3);
-    vel(1) = state(1);
-    vel(2) = state(2);
+    vel(1) = state(4);
+    vel(2) = state(5);
     vel(3) = 0;
 
     MatrixWrapper::Matrix zCross(3,3);
@@ -104,7 +104,7 @@ WheelPodMeasurementPdf::ExpectedValueGet(void) const
     MatrixWrapper::Matrix pod_direction = computePodDirections();
     MatrixWrapper::Matrix pod_direction_perp = zCross*pod_direction;
 
-    MatrixWrapper::Matrix pod_circular_vel = zCross*pod_positions*state(3);
+    MatrixWrapper::Matrix pod_circular_vel = zCross*pod_positions*state(6);
 
     MatrixWrapper::ColumnVector podvel;
     podvel = (vel + pod_circular_vel.columnCopy(1));
