d1329b3f3a99e4a5399d61c0c43ce51f690d1061
zac <zlizer@appliedminds.com>
2013-04-25 19:54:11 -0700

    Added disparity callback, removed unnecessary filename declaration

--- ros_workspace/src/linemod_detector/src/linemod_detector.cpp
+++ ros_workspace/src/linemod_detector/src/linemod_detector.cpp
@@ -56,6 +56,7 @@ class LineMOD_Detector
   image_transport::ImageTransport it;
   image_transport::Subscriber color_sub;
   ros::Subscriber depth_sub;
+  ros::Subscriber disp_sub;
   std::vector<cv::Mat> sources;
   cv::Mat color_img;
   cv::Mat display;
@@ -69,7 +70,8 @@ class LineMOD_Detector
   LineMOD_Detector(): it(nh)
   {
     color_sub = it.subscribe("color", 1, &LineMOD_Detector::colorCallback, this);
-    depth_sub = nh.subscribe("disparity", 1, &LineMOD_Detector::depthCallback, this);
+    depth_sub = nh.subscribe("depth", 1, &LineMOD_Detector::depthCallback, this);
+    disp_sub = nh.subscribe("disparity", 1, &LineMOD_Detector::disparityCallback, this);
     matching_threshold = 80;
     got_color = false;
 
@@ -81,14 +83,73 @@ class LineMOD_Detector
     std::cout << num_modalities << std::endl;
   }
 
+  void disparityCallback(const stereo_msgs::DisparityImageConstPtr& msg)
+  {
+    if (!LineMOD_Detector::got_color)
+    {
+      return;
+    }
+    bool show_match_result = true;
+    cv_bridge::CvImagePtr disp_ptr;
+    try
+    {
+      disp_ptr = cv_bridge::toCvCopy(msg->image, "");
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("disp cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    float f = msg->f;
+    float T = msg->T;
+    cv::Mat depth_img = (f*T*1000)/(disp_ptr->image).clone();
+    depth_img.convertTo(depth_img, CV_16U);
+    LineMOD_Detector::sources.push_back(LineMOD_Detector::color_img);
+    LineMOD_Detector::sources.push_back(depth_img);
+
+      // Perform matching
+      std::vector<cv::linemod::Match> matches;
+      std::vector<std::string> class_ids;
+      std::vector<cv::Mat> quantized_images;
+
+      LineMOD_Detector::detector->match(sources, (float)LineMOD_Detector::matching_threshold, matches, class_ids, quantized_images);
+
+      LineMOD_Detector::num_classes = detector->numClasses();
+      int classes_visited = 0;
+      std::set<std::string> visited;
+
+      for (int i = 0; (i < (int)matches.size()) && (classes_visited < LineMOD_Detector::num_classes); ++i)
+      {
+        cv::linemod::Match m = matches[i];
+
+        if (visited.insert(m.class_id).second)
+        {
+          ++classes_visited;
+
+          if (show_match_result)
+          {
+            printf("Similarity: %5.1f%%; x: %3d; y: %3d; class: %s; template: %3d\n",
+                   m.similarity, m.x, m.y, m.class_id.c_str(), m.template_id);
+          }
+
+          // Draw matching template
+          const std::vector<cv::linemod::Template>& templates = LineMOD_Detector::detector->getTemplates(m.class_id, m.template_id);
+          drawResponse(templates, LineMOD_Detector::num_modalities, LineMOD_Detector::display, cv::Point(m.x, m.y), LineMOD_Detector::detector->getT(0));
+
+        }
+      }
+
+      LineMOD_Detector::sources.clear();
+  }
+
+
   void depthCallback(const sensor_msgs::ImageConstPtr& msg)
   {
     if (!LineMOD_Detector::got_color)
     {
       return;
     }
-    std::string filename;
-    filename = "test";
     cv_bridge::CvImagePtr depth_ptr;
     bool show_match_result = true;
     try
