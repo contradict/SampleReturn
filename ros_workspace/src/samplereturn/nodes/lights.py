b4adb80f698aaaf05beb54ce1ea7ec91dc3a86b5
Jascha <jtlittle@gmail.com>
2013-05-09 23:21:36 -0700

    Pause state is now also displayed by safety lights

--- ros_workspace/src/samplereturn/nodes/lights.py
+++ ros_workspace/src/samplereturn/nodes/lights.py
@@ -4,20 +4,34 @@ import roslib; roslib.load_manifest('samplereturn')
 import rospy
 
 from platform_motion.msg import GPIO
+from std_msgs.msg import Bool
 
-class blinker(object):
+class LightController(object):
+    _pin_mask = 7 #mask that defines the light pins
+    _all_on = 0 #bit states for all lights on
+    _all_off = 7 #bit states for all lights off
+     
     def __init__(self):
+        rospy.Subscriber("/pause_state", Bool, self.pause_state_update)
         self.pub = rospy.Publisher('gpio_write', GPIO)
-        self.light_state = 1
-        self.cycle = rospy.Timer(rospy.Duration(0.1), self.shift_lights)
+        self.blink = False
+        self.light_state = self._all_on
+        self.cycle = rospy.Timer(rospy.Duration(1.0), self.set_lights)
 
-    def shift_lights(self, event):
-        self.pub.publish(GPIO(servo_id=5,pin_mask=7,new_pin_states=self.light_state))
-        self.light_state = ((self.light_state*2)&0x7) + ((self.light_state&0x4)/4)
-
+    def set_lights(self, event): #rospy.Timer passes in an event object to this cb
+        if self.blink:
+            self.light_state = self._all_on if (self.light_state == self._all_off)\
+                               else self._all_off
+        else:
+            self.light_state = self._all_on
+        self.pub.publish(GPIO(servo_id=1, pin_mask=7, new_pin_states=self.light_state))
+        
+    def pause_state_update(self, pause_state_msg):
+        self.blink = pause_state_msg.data
+        
 def main():
-    rospy.init_node('blinker')
-    b = blinker()
+    rospy.init_node('lights')
+    b = LightController()
     rospy.spin()
 
 if __name__=="__main__":
