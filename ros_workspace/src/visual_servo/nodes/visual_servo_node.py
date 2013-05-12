7be3235844d68ac54538c181ea0ab2e3b9c566d8
contradict <contradict@gmail.com>
2013-05-12 11:29:04 -0700

    Switch visual_servo camera info to manipulator

--- ros_workspace/src/visual_servo/nodes/visual_servo_node.py
+++ ros_workspace/src/visual_servo/nodes/visual_servo_node.py
@@ -60,8 +60,8 @@ class VisualServo:
 		self.reset()
 
 		rospy.Subscriber('/img_point', NamedPoint, self.object_point_callback, None, 1)
-		rospy.Subscriber('/navigation/left/camera_info', CameraInfo, self.camera_left_info_callback, None, 1)
-		rospy.Subscriber('/navigation/right/camera_info', CameraInfo, self.camera_right_info_callback, None, 1)
+		rospy.Subscriber('/manipulator/left/camera_info', CameraInfo, self.camera_left_info_callback, None, 1)
+		rospy.Subscriber('/manipulator/right/camera_info', CameraInfo, self.camera_right_info_callback, None, 1)
 		rospy.Subscriber('/debug/debug_update_constants', TunableConstants, self.debug_update_constants, None, 1)
 		self._publisher = rospy.Publisher('/servo_command', Twist)
 
