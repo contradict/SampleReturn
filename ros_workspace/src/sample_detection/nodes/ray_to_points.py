b05adef6577514ab5aa3d3c69c7d9450d3374c39
zac <zlizer@appliedminds.com>
2013-05-23 20:42:41 -0700

    This now appears to place the center point correctly on the ground

--- ros_workspace/src/sample_detection/nodes/ray_to_points.py
+++ ros_workspace/src/sample_detection/nodes/ray_to_points.py
@@ -18,7 +18,7 @@ class ray_to_points(object):
   # geometrical uncertainty (most notably, pitch)
 
   def __init__(self):
-    rospy.init_node('ray_to_points',anonymous=True)
+    rospy.init_node('ray_to_points',anonymous=True,log_level=rospy.DEBUG)
 
     self.named_point_sub = rospy.Subscriber('named_point', NamedPoint, self.handle_named_point)
 
@@ -27,20 +27,59 @@ class ray_to_points(object):
 
     self.tf = TransformListener()
 
-  def handle_named_point(self, NamedPoint):
-    base_link_point = self.tf.transformPoint('/base_link', NamedPoint.point)
-    t = self.tf.getLatestCommonTime('/base_link', NamedPoint.header.frame_id)
-    pos, quat = self.tf.lookupTransform('/base_link', NamedPoint.header.frame_id, t)
-    height = pos[2]
-    ground_point = base_link_point*np.abs(height)
+    self.pitch_error = rospy.get_param("~pitch_error",0.1)
+    self.yaw_error = rospy.get_param("~yaw_error",0.1)
 
-    #self.points_pub.publish
+  def handle_named_point(self, point_in):
+    rospy.logdebug("handle_named_point x: %s y: %s z: %s",
+        point_in.point.x,
+        point_in.point.y,
+        point_in.point.z)
+    point_stamped = PointStamped()
+    point_stamped.header = point_in.header
+    point_stamped.point = point_in.point
+
+    base_link_point = self.tf.transformPoint('/base_link', point_stamped)
+
+    t = self.tf.getLatestCommonTime('/base_link', point_in.header.frame_id)
+    pos, quat = self.tf.lookupTransform('/base_link', point_in.header.frame_id, t)
+    rospy.logdebug("pos: %s", pos)
+    rospy.logdebug("quat: %s", quat)
+    height = pos[2]
+
+    x_slope = np.abs((pos[0]-base_link_point.point.x)/(pos[2]-base_link_point.point.z))
+    y_slope = np.abs((pos[1]-base_link_point.point.y)/(pos[2]-base_link_point.point.z))
+
+    ground_point = np.array([0.,0.,0.])
+    ground_point[0] = x_slope*height
+    ground_point[1] = y_slope*height
+
+    ground_named_point = NamedPoint()
     ground_named_point.point.x = ground_point[0]
     ground_named_point.point.y = ground_point[1]
     ground_named_point.point.z = ground_point[2]
-    ground_named_point.header = NamedPoint.header
-    ground_named_point.name = NamedPoint.name
+    ground_named_point.header = point_in.header
+    ground_named_point.name = point_in.name
+    rospy.logdebug("ground_named_point %s",ground_named_point)
     self.named_point_pub.publish(ground_named_point)
+
+  def make_point_cloud(Point):
+    # Take a vector, nominally [x,y,1] and apply some rotation about x (pitch)
+    # and about y (yaw) in the base_link frame. This will make a frustum that
+    # can be sampled for a point cloud
+    p = self.pitch_error
+    pitch_mat = np.array([[1., 0., 0.],[0., np.cos(p), -np.sin(p)],[0., np.sin(p), np.cos(p)]])
+    y = self.yaw_error
+    yaw_mat = np.array([[np.cos(y), 0., np.sin(y)],[0., 1., 0.],[-np.sin(y), 0., np.cos(y)]])
+    vec = np.array([0,0,0])
+    vec[0] = Point.x
+    vec[1] = Point.y
+    vec[2] = Point.z
+    down_left = np.dot(pitch_mat,np.dot(yaw_mat,vec))
+    down_right = np.dot(pitch_mat,np.dot(-yaw_mat,vec))
+    up_left = np.dot(-pitch_mat,np.dot(yaw_mat,vec))
+    up_right = np.dot(-pitch_mat,np.dot(-yaw_mat,vec))
+
 
 
 if __name__=="__main__":
