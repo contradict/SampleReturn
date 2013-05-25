4dbbd8420ca0680b4bba4feed850c5f9ddcd3f7c
contradict <contradict@gmail.com>
2013-05-25 09:13:21 -0700

    Detection filter
    A detection has to stay in the same place for a while to be valid

---
+++ ros_workspace/src/detection_filter/nodes/detection_filter_node.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+
+from detection_filter import Filter, Hypothesis
+from linemod_detector.msg import NamedPoint
+
+class SampleDetectionFilter(object):
+    def __init__(self):
+        # node parameters
+        self.sample_name = rospy.get_param("~sample_name", "pre_cached")
+
+        self.filter_tolerance = rospy.get_param("~filter_tolerance", 0.1)
+        self.filter_alpha = rospy.get_param("~filter_alpha", 0.25)
+        self.filter_unsupported_step = rospy.get_param("~filter_unsupported_step", 0.5)
+        self.filter_threshold = rospy.get_param("~filter_threshold", 5)
+        self.filter_frame = rospy.get_param("~filter_frame", "odom")
+
+        self.filter = Filter(
+                Hypothesis(tolerance=self.filter_tolerance,
+                           alpha=self.filter_alpha,
+                           unsupported_step=self.filter_unsupported_step),
+                self.filter_frame)
+
+        rospy.Subscriber('raw_points', NamedPoint, queue_size=1,
+                callback=self.handle_point)
+
+        self.publisher = rospy.Publisher('filtered_points', NamedPoint)
+
+    def handle_point(self, msg):
+        if msg.name == self.sample_name:
+            point = np.array([msg.point.x, msg.point.y, msg.point.z])
+            self.filter.update(point, msg.header.frame_id)
+            e = self.filter.estimate(self.filter_threshold)
+            if e is not None:
+                fp=NamedPoint()
+                fp.header = msg.header
+                fp.point.x = e.position[0]
+                fp.point.y = e.position[1]
+                fp.point.z = e.position[2]
+                fp.name = self.sample_name
+                self.publisher.publish(fp)
+
+def start_node():
+    rospy.init_node('sample_detection_filter')
+    filter = SampleDetectionFilter()
+    rospy.spin()
+
+if __name__=="__main__":
+    start_node()
---
+++ ros_workspace/src/detection_filter/src/detection_filter/filter.py
@@ -0,0 +1,90 @@
+import tf
+import rospy
+import numpy as np
+
+class Hypothesis(object):
+    def __init__(self, position=None, tolerance=0.1, alpha=0.25, unsupported_step=0.5):
+        self.position = position
+        self.tolerance = tolerance
+        self.alpha = alpha
+        self.unsupported_step = unsupported_step
+        if self.position is not None:
+            self.support = 1
+        else:
+            self.support = 0
+
+    def supports(self, position):
+        if self.position is None:
+            return True
+        return self.distance_to(position)<self.tolerance
+
+    def distance_to(self, position):
+        return np.sqrt(((self.position[:2]-position[:2])**2).sum())
+
+    def update(self, position):
+        if self.position is None:
+            self.position = position
+            self.support = 1
+        else:
+            self.position = (1.0-self.alpha)*self.position + self.alpha*position
+            self.support += 1
+
+    def unsupported(self):
+        self.support -= self.unsupported_step
+
+    def valid(self, threshold):
+        return self.support>threshold
+
+    def replace(self, other):
+        self.position = other.position
+        self.support = other.support
+
+    def __lt__(self, other):
+        return self.support<other.support
+
+    def __call__(self, position=None):
+        h=Hypothesis(position=position, tolerance=self.tolerance, alpha=self.alpha,
+                unsupported_step=self.unsupported_step)
+        return h
+
+class Filter(object):
+    def __init__(self, hypothesis, frame="map", hypotheses=3):
+        self.frame = frame
+        self.hypothesis = hypothesis
+
+        self.hypotheses=[self.hypothesis()]*hypotheses
+
+        self.listener = tf.TransformListener()
+
+    def update(self, point, frame):
+        latest = rospy.Time(0)
+        if not self.listener.canTransform(self.frame, frame, latest):
+            rospy.logerr("Can not transform frame %s to %s, ignoring measurement", frame, self.frame)
+            return
+        translation, rotation = self.listener.lookupTransform(self.frame, frame,
+                                                             latest)
+        point += np.array(translation)
+        worst=self.hypotheses[0]
+        for h in self.hypotheses:
+            if h.supports(point):
+                h.update(point)
+                break
+            else:
+                h.unsupported()
+            if h<worst:
+                worst = h
+        else:
+            worst.replace(self.hypothesis(position))
+
+    def estimate(self, threshold=None):
+        best = self.hypotheses[0]
+        if best is None:
+            return None
+        for h in self.hypotheses:
+            if best<h:
+                best = h
+        if threshold is None or best.valid(threshold):
+            return best
+        else:
+            return None
+
