bff54f049916561c1a1fae1d090fdd790de48cac
contradict <contradict@gmail.com>
2013-06-04 08:06:43 -0700

    Correct duration test again

--- ros_workspace/src/executive/nodes/master_executive.py
+++ ros_workspace/src/executive/nodes/master_executive.py
@@ -261,9 +261,9 @@ class SampleReturnScheduler(teer_ros.Scheduler):
 
     def servo_feedback_cb(self, feedback):
         now = rospy.Time.now()
+        dt = (now-self.last_servo_feedback)
         if self.last_servo_feedback is None or \
-            (len(self.proclamations) == 0 and \
-              ((now-self.last_servo_feedback).to_sec()>self.servo_feedback_interval)):
+            (len(self.proclamations) == 0 and dt>self.servo_feedback_interval):
             if feedback.state == feedback.STOP_AND_WAIT:
                 self.announce("lost sample")
             else:
