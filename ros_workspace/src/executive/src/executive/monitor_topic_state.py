fc98dabb986d8432759ac95ebc19cd0bba47f0ea
Jascha <jtlittle@gmail.com>
2014-02-06 23:27:41 -0800

    Added timeout, removed extra debug statements.

--- ros_workspace/src/executive/src/executive/monitor_topic_state.py
+++ ros_workspace/src/executive/src/executive/monitor_topic_state.py
@@ -11,12 +11,6 @@ class MonitorTopicState(smach.State):
     """A state that checks a field in a given ROS topic, and compares against specified
     values.  Each specified value tiggers an outcome of the state.
     """
-    
-    #TopicValueOutcome = namedtuple('TopicValueOutcome', 'value outcome threshold')
-    #topic value outcomes is a list of named tuples.  The state will exit with the outcome
-    #corresponding to a topic value once the state receives that value threshold times.
-    #if a timeout is specified, the state will exit with the outcome 'timeout'
- 
     def __init__(self, topic, field, msg_type, topic_value_outcomes, timeout=None):
         
         self._values = []
@@ -50,11 +44,9 @@ class MonitorTopicState(smach.State):
         self._sub = rospy.Subscriber(self._topic, self._msg_type, self._sub_cb)
 
         self._trigger_cond.acquire()
-        self._trigger_cond.wait()
+        self._trigger_cond.wait(timeout=self._timeout)
         self._trigger_cond.release()
         
-        rospy.loginfo('Past thread lock')
-
         self._sub.unregister()
 
         if self._triggered_outcome is not None:
@@ -68,11 +60,7 @@ class MonitorTopicState(smach.State):
 
     #the topic subscriber callback
     def _sub_cb(self, msg):
-        rospy.loginfo('Monitor State callback triggered with: ' + getattr(msg, self._field))
-        rospy.loginfo("Values: " + str(self._values))
-        rospy.loginfo("Values: " + str(self._outcome_list))
         if self._value_check(getattr(msg, self._field)):
-            rospy.loginfo('That shit was true')
             self._trigger_cond.acquire()
             self._trigger_cond.notify()
             self._trigger_cond.release()
@@ -81,8 +69,6 @@ class MonitorTopicState(smach.State):
     def _value_check(self, check_value):
         if check_value in self._values:
             i = self._values.index(check_value)
-            rospy.loginfo('Index of match: ' + str(i))
-            rospy.loginfo('Counts: ' + str(self._value_counts[i]) + ", Threshold: " + str(self._thresholds[i]))
             self._value_counts[i] += 1
             if (self._value_counts[i] >= self._thresholds[i]):
                 self._triggered_outcome = self._outcome_list[i]
