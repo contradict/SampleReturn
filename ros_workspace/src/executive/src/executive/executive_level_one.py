a1c258935f715a2c2ef1c49279c94476ec2239fe
Jascha <jtlittle@gmail.com>
2014-03-30 21:43:03 -0700

    Preemption handling added to test state.

--- ros_workspace/src/executive/src/executive/executive_level_one.py
+++ ros_workspace/src/executive/src/executive/executive_level_one.py
@@ -2,13 +2,18 @@ import smach
 
 #the top level executive passes the level one state machine object to this
 #function.  This is where its states are added
-def AddStates(state_machine):
+def AddStates(state_machine, announcer):
     
     with state_machine:
         
+            smach.StateMachine.add('START_LEVEL_ONE',
+                                   StartLevelOne(announcer),
+                                   transitions = {'next':'DRIVE_TO_SAMPLE'})
+            
             smach.StateMachine.add('DRIVE_TO_SAMPLE',
                                    DriveToSample(),
-                                   transitions = {'sample_detected':'PURSUE_SAMPLE'})
+                                   transitions = {'sample_detected':'PURSUE_SAMPLE',
+                                                  'preempted':'LEVEL_ONE_PREEMPTED'})
             
             smach.StateMachine.add('PURSUE_SAMPLE',
                                    PursueSample(),
@@ -25,12 +30,29 @@ def AddStates(state_machine):
                                                   'fail':'aborted'})  
     
 #drives to the expected sample location    
+class StartLevelOne(smach.State):
+    def __init__(self, announcer):
+        smach.State.__init__(self,
+                             outcomes=['next'])
+        
+        self.announcer = announcer
+        
+    def execute(self, userdata):
+        
+        self.announcer.say("Entering level one mode")
+
 class DriveToSample(smach.State):
     def __init__(self):
         smach.State.__init__(self,
-                             outcomes=['sample_detected']
+                             outcomes=['sample_detected', 'preempted']
                             )
     def execute(self, userdata):
+        
+        while True:
+            rospy.sleep(0.1)
+            if self.preempt_requested():
+                self.service_preempt()
+                return 'preempted'
         
         return 'sample_detected'
 
@@ -48,8 +70,10 @@ class PursueSample(smach.State):
 class LevelOnePreempted(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['complete','fail'])
-        
+          
     def execute(self):
+        
+        #shutdown all internal processes to level_one
         
         return 'complete'
 
