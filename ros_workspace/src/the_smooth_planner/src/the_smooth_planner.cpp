d0a43f54a8bfd96a20d76989b3ea86dc652a9021
Brandon Booth <brandon.m.booth@gmail.com>
2014-04-12 15:59:19 -0700

    Removed marker visualization message
    Russel is writing a rviz plugin to visualize these path messages,
    which will be useful outside of just this planner

--- ros_workspace/src/the_smooth_planner/src/the_smooth_planner.cpp
+++ ros_workspace/src/the_smooth_planner/src/the_smooth_planner.cpp
@@ -3,7 +3,6 @@
 #include <costmap_2d/inflation_layer.h>
 #include <ecl/geometry/cubic_spline.hpp>
 #include <platform_motion_msgs/Path.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <tf/LinearMath/Quaternion.h>
 #include <tf/LinearMath/Matrix3x3.h>
 
@@ -43,7 +42,6 @@ void TheSmoothPlanner::initialize(std::string name, tf::TransformListener* tf, c
 	odom_subscriber = parentNodeHandle.subscribe("odometry", 1, &TheSmoothPlanner::setOdometry, this);
 
 	smooth_path_publisher = localNodeHandle.advertise<platform_motion_msgs::Path>("/motion/planned_path", 1);
-	visualization_publisher = localNodeHandle.advertise<visualization_msgs::MarkerArray>("visualization_marker_array", 0);
 }
 
 bool TheSmoothPlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
@@ -84,11 +82,6 @@ void TheSmoothPlanner::setPath(const nav_msgs::Path& path)
 	path_msg.header.seq = 0;
 	path_msg.header.frame_id = "map";
 
-	visualization_msgs::MarkerArray visualizationMarkerArray;
-	visualizationMarkerArray.markers.resize(1); // Just a single marker for now
-	visualizationMarkerArray.markers[0].points.resize(path.poses.size());
-	visualizationMarkerArray.markers[0].colors.resize(path.poses.size());
-
 	// Compute the distance we should travel before we stop accelerating
 	double stopAcceleratingDistance = (maximum_linear_velocity*maximum_linear_velocity - currentVelocityMagnitude*currentVelocityMagnitude)/(2.0*linear_acceleration);
 	double distanceTraveled = 0.0;
@@ -101,22 +94,6 @@ void TheSmoothPlanner::setPath(const nav_msgs::Path& path)
 		path_msg.knots[0].header.frame_id = "map";
 		path_msg.knots[0].pose = path.poses[0].pose;
 		path_msg.knots[0].twist = odometry.twist.twist;
-
-		visualizationMarkerArray.markers[0].header.seq = 0;
-		visualizationMarkerArray.markers[0].header.frame_id = "map";
-		visualizationMarkerArray.markers[0].header.stamp = timestamp;
-		visualizationMarkerArray.markers[0].ns = "SmoothPlannerVisualization";
-		visualizationMarkerArray.markers[0].id = 0;
-		visualizationMarkerArray.markers[0].type = visualization_msgs::Marker::LINE_STRIP;
-		visualizationMarkerArray.markers[0].action = visualization_msgs::Marker::ADD;
-		visualizationMarkerArray.markers[0].scale.x = 0.1;
-		visualizationMarkerArray.markers[0].scale.y = 1.0; // Not used
-		visualizationMarkerArray.markers[0].scale.z = 1.0; // Not used
-		visualizationMarkerArray.markers[0].points[0] = path.poses[0].pose.position;
-		visualizationMarkerArray.markers[0].colors[0].r = 1.0 - pathVelocityMagnitude/maximum_linear_velocity;
-		visualizationMarkerArray.markers[0].colors[0].g = pathVelocityMagnitude/maximum_linear_velocity;
-		visualizationMarkerArray.markers[0].colors[0].b = 0.0;
-		visualizationMarkerArray.markers[0].colors[0].a = 1.0;
 	}
 
 	for (unsigned int i = 0; i < path.poses.size()-1; ++i)
@@ -187,13 +164,6 @@ void TheSmoothPlanner::setPath(const nav_msgs::Path& path)
 		path_msg.knots[i+1].twist.angular.y = 0.0;
 		path_msg.knots[i+1].twist.angular.z = 0.0;
 
-		// Store the position for the line strip
-		visualizationMarkerArray.markers[0].points[i+1] = path.poses[i+1].pose.position;
-		visualizationMarkerArray.markers[0].colors[i+1].r = 1.0 - finalLinearVelocityMagnitude/maximum_linear_velocity;
-		visualizationMarkerArray.markers[0].colors[i+1].g = finalLinearVelocityMagnitude/maximum_linear_velocity;
-		visualizationMarkerArray.markers[0].colors[i+1].b = 0.0;
-		visualizationMarkerArray.markers[0].colors[i+1].a = 1.0;
-
 		pathVelocityMagnitude = finalLinearVelocityMagnitude;
 	}
 
@@ -212,8 +182,6 @@ void TheSmoothPlanner::setPath(const nav_msgs::Path& path)
 		// Update the velocity for this path point
 		path_msg.knots[i].twist.linear.x *= targetDecelerationVelocity;
 		path_msg.knots[i].twist.linear.y *= targetDecelerationVelocity;
-		visualizationMarkerArray.markers[0].colors[i].r = 1.0 - targetDecelerationVelocity/maximum_linear_velocity;
-		visualizationMarkerArray.markers[0].colors[i].g = targetDecelerationVelocity/maximum_linear_velocity;
 
 		double delta_time_seconds = (path_msg.knots[i].header.stamp.sec-path_msg.knots[i-1].header.stamp.sec) +
                                     (path_msg.knots[i].header.stamp.nsec-path_msg.knots[i-1].header.stamp.nsec)/1000000000.0;
@@ -223,7 +191,6 @@ void TheSmoothPlanner::setPath(const nav_msgs::Path& path)
 
 	// 6) Publish the_smooth_path and visualization messages
 	smooth_path_publisher.publish(path_msg);
-	visualization_publisher.publish(visualizationMarkerArray);
 	return;
 }
 
