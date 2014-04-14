a7606fa65f89e453013da04f8f71c6a7af457818
zac <zachary.lizer@gmail.com>
2014-04-13 18:57:33 -0700

    Add better color computation
    When computing interior color, exclude pixels that are too similar to
    background color

--- ros_workspace/src/saliency_detector/include/color_naming.h
+++ ros_workspace/src/saliency_detector/include/color_naming.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <fstream>
 
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <ros/console.h>
+
 /* Load color matrix
  * Compute index
  * Compute interior color
@@ -19,6 +23,8 @@ class ColorNaming
     cv::Mat computeIndexImage(cv::Mat image);
     Eigen::Matrix<float,11,1> computeInteriorColor(cv::Mat image, cv::Mat mask);
     Eigen::Matrix<float,11,1> computeExteriorColor(cv::Mat image, cv::Mat mask);
+    Eigen::Matrix<float,11,1> computeInteriorColor(cv::Mat image,
+        cv::Mat mask, Eigen::Matrix<float,11,1> exteriorColor);
   private:
     float color_index_array_ [458752];
 };
