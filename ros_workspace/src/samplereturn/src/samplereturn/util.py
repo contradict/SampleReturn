fd0d60045f0240765115207dc1266982ed8248ac
Jascha <jtlittle@gmail.com>
2014-04-29 17:56:52 -0700

    Added get_current_robot_pose to util

--- ros_workspace/src/samplereturn/src/samplereturn/util.py
+++ ros_workspace/src/samplereturn/src/samplereturn/util.py
@@ -1,7 +1,14 @@
 import rospy
+import math
 import rosgraph_msgs.msg as rosgraph_msg
 from collections import namedtuple
 from samplereturn_msgs.msg import VoiceAnnouncement
+
+import geometry_msgs.msg as geometry_msg
+import actionlib_msgs.msg as action_msg
+
+
+actionlib_working_states = [action_msg.GoalStatus.ACTIVE, action_msg.GoalStatus.PENDING]
 
 class AnnouncerInterface(object):
     def __init__(self, topic):
@@ -40,4 +47,30 @@ def get_node_params():
     node_params_dict = rospy.get_param(rospy.get_name())
     ParamTuple =  namedtuple('ParamTuple', node_params_dict.keys())
     return ParamTuple(**node_params_dict)
+
+#takes two std_msg.Point objects, and returns the xy plane distance
+def point_distance_2d(pt1, pt2):
+    return math.sqrt((pt1.x-pt2.x)**2 + (pt1.y-pt2.y)**2)
+
+def get_current_robot_pose(tf_listener):
+    tf_listener.waitForTransform('/map',
+                                 '/base_link',
+                                 rospy.Time(0),
+                                 rospy.Duration(1.0))
+    
+    now = tf_listener.getLatestCommonTime('/map', '/base_link')
+    
+    pos, quat = tf_listener.lookupTransform('/map',
+                                            '/base_link',
+                                            rospy.Time(0))
+    
+    header = std_msg.Header(0, now, '/map')
+    
+    pose = geometry_msg.PoseStamped(header,
+                                    geometry_msg.Pose(geometry_msg.Point(*pos),
+                                                      geometry_msg.Quaternion(*quat)
+                                                      )
+                                    )
+    return pose
+
 
