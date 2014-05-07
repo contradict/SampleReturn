db48e0586656bfb8b0465132e2d04ec10ab81346
Jascha <jtlittle@gmail.com>
2014-05-06 19:59:55 -0700

    Added pose translation/rotation tools to util

--- ros_workspace/src/samplereturn/src/samplereturn/util.py
+++ ros_workspace/src/samplereturn/src/samplereturn/util.py
@@ -1,9 +1,12 @@
 import rospy
 import math
-import rosgraph_msgs.msg as rosgraph_msg
-from collections import namedtuple
-from samplereturn_msgs.msg import VoiceAnnouncement
+import tf
 
+from collections import namedtuple
+
+from samplereturn_msgs.msg import VoiceAnnouncement
+import rosgraph_msgs.msg as rosgraph_msg
+import std_msgs.msg as std_msg
 import geometry_msgs.msg as geometry_msg
 import actionlib_msgs.msg as action_msg
 
@@ -52,25 +55,50 @@ def get_node_params():
 def point_distance_2d(pt1, pt2):
     return math.sqrt((pt1.x-pt2.x)**2 + (pt1.y-pt2.y)**2)
 
+#takes two std_msg.PoseStamped objects, and returns the xy plane distance
+def pose_distance_2d(pose1, pose2):
+    return math.sqrt((pose1.pose.position.x-pose2.pose.position.x)**2 + \
+                     (pose1.pose.position.y-pose2.pose.position.y)**2)
+
+#takes a stamped pose and returns a stamped pose distance forward in /map frame
+def pose_translate(listener, start_pose, dx, dy):
+    rospy.loginfo('start pose: %s', start_pose)
+    header = std_msg.Header(0, rospy.Time(0), '/base_link')
+    new_base_point = geometry_msg.PointStamped(header,
+                                               geometry_msg.Point(dx, dy, 0))
+    new_point = listener.transformPoint('/map', new_base_point)
+    rospy.loginfo('new point: %s %s', dx, dy)
+    new_pose = geometry_msg.Pose(new_point.point, start_pose.pose.orientation)
+    header = std_msg.Header(0, rospy.Time(0), '/map')
+    rospy.loginfo('new pose: %s', new_pose)
+    return geometry_msg.PoseStamped(header, new_pose)    
+    
+def pose_rotate(listener, start_pose, angle):
+    #rospy.loginfo('start pose: %s', start_pose)
+    goal_quat = tf.transformations.quaternion_from_euler(0,0,math.radians(angle))
+    start_quat = (start_pose.pose.orientation.x,
+                  start_pose.pose.orientation.y,
+                  start_pose.pose.orientation.z,
+                  start_pose.pose.orientation.w)
+    
+    new_quat = tf.transformations.quaternion_multiply(goal_quat, start_quat)
+    new_orientation = geometry_msg.Quaternion(*new_quat)
+    new_pose = geometry_msg.Pose(start_pose.pose.position, new_orientation)
+    header = std_msg.Header(0, rospy.Time(0), '/map')
+    #rospy.loginfo('rotate %f', angle)
+    #rospy.loginfo('new pose: %s', new_pose)
+    return geometry_msg.PoseStamped(header, new_pose)    
+
+
 def get_current_robot_pose(tf_listener):
-    tf_listener.waitForTransform('/map',
-                                 '/base_link',
-                                 rospy.Time(0),
-                                 rospy.Duration(1.0))
-    
     now = tf_listener.getLatestCommonTime('/map', '/base_link')
-    
     pos, quat = tf_listener.lookupTransform('/map',
                                             '/base_link',
                                             rospy.Time(0))
-    
     header = std_msg.Header(0, now, '/map')
+    pose = geometry_msg.Pose(geometry_msg.Point(*pos),
+                             geometry_msg.Quaternion(*quat))
+                                    
+    return geometry_msg.PoseStamped(header, pose)
     
-    pose = geometry_msg.PoseStamped(header,
-                                    geometry_msg.Pose(geometry_msg.Point(*pos),
-                                                      geometry_msg.Quaternion(*quat)
-                                                      )
-                                    )
-    return pose
-
 
