94aa5b87d53a0e0aa72fadc8f05dc55270bbb60d
zac <zachary.lizer@gmail.com>
2014-05-13 10:04:37 -0700

    Fix predicting of Kalman filter

--- ros_workspace/src/detection_filter/nodes/kalman_filter_node.cpp
+++ ros_workspace/src/detection_filter/nodes/kalman_filter_node.cpp
@@ -7,7 +7,6 @@
 #include <ros/ros.h>
 #include <ros/time.h>
 #include <ros/console.h>
-//#include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <samplereturn_msgs/NamedPoint.h>
 
@@ -66,39 +65,36 @@ class KalmanDetectionFilter
       }
     }
     checkObservation(msg);
-    //else {
-      //if (checkObservation) {
-      //  addFilter(msg);
-      //  return;
-      //}
-      //else {
-      //  addMeasurement(msg);
-      //  return;
-      //}
   }
 
   void addFilter(const samplereturn_msgs::NamedPoint& msg)
   {
     std::shared_ptr<cv::KalmanFilter> KF (new cv::KalmanFilter(4,2));
-    //cv::KalmanFilter KF(4,2);
     cv::Mat state(4, 1, CV_32F); /* x, y, vx, vy */
     cv::Mat processNoise(4, 1, CV_32F);
 
+    KF->transitionMatrix = (cv::Mat_<float>(4,4) << 1, 0, 0.03, 0,
+                                                    0, 1, 0, 0.03,
+                                                    0, 0, 1, 0,
+                                                    0, 0, 0, 1);
     cv::setIdentity(KF->measurementMatrix);
-    cv::setIdentity(KF->processNoiseCov, cv::Scalar(1e0));
-    cv::setIdentity(KF->measurementNoiseCov, cv::Scalar(1e1));
-    cv::setIdentity(KF->errorCovPost, cv::Scalar(1));
+    cv::setIdentity(KF->processNoiseCov, cv::Scalar(5e0));
+    cv::setIdentity(KF->measurementNoiseCov, cv::Scalar(2e1));
+    cv::setIdentity(KF->errorCovPost, cv::Scalar(1e1));
 
     KF->statePost.at<float>(0) = msg.point.x;
     KF->statePost.at<float>(1) = msg.point.y;
     KF->statePost.at<float>(2) = 0;
     KF->statePost.at<float>(3) = 0;
 
+    KF->predict();
     filter_list_.push_back(KF);
+    checkObservation(msg);
   }
 
   void addMeasurement(const cv::Mat meas_state, int filter_index)
   {
+    ROS_INFO("Adding measurement to filter: %i", filter_index);
     filter_list_[filter_index]->correct(meas_state);
   }
 
@@ -116,14 +112,13 @@ class KalmanDetectionFilter
         addMeasurement(meas_state, i);
         return;
       }
-      addFilter(msg);
     }
+    addFilter(msg);
   }
 
   /* The process tick for all filters */
   void cameraInfoCallback(const sensor_msgs::CameraInfo& msg)
   {
-    //ROS_INFO("Camera Info Callback");
     if (filter_list_.size() == 0) {
       return;
     }
@@ -131,6 +126,7 @@ class KalmanDetectionFilter
       last_time_ = msg.header.stamp;
       for (int i=0; i<filter_list_.size(); i++) {
         filter_list_[i]->predict();
+        filter_list_[i]->errorCovPre.copyTo(filter_list_[i]->errorCovPost);;
       }
     }
     drawFilterStates();
@@ -138,22 +134,28 @@ class KalmanDetectionFilter
 
   void drawFilterStates() {
     std::cout << "Number of Filters: " << filter_list_.size() << std::endl;
-    cv::Mat img(500, 500, CV_8UC3);
-    //for (auto filter_ptr : filter_list_){
-    for (int i=0; i<filter_list_.size(); i++) {
-      ROS_INFO("Drawing...");
-      std::cout << "Doin' it" << std::endl;
-      cv::Point mean(filter_list_[i]->statePost.at<float>(0),
-          filter_list_[i]->statePost.at<float>(1));
-      std::cout << "Drawn Point: " << mean << std::endl;
-      float rad_x =  filter_list_[i]->errorCovPost.at<float>(0,0);
-      float rad_y =  filter_list_[i]->errorCovPost.at<float>(1,1);
+    cv::Mat img = cv::Mat::zeros(500, 500, CV_8UC3);
+    for (auto filter_ptr : filter_list_){
+      cv::Point mean(filter_ptr->statePost.at<float>(0),
+          filter_ptr->statePost.at<float>(1));
+      float rad_x = filter_ptr->errorCovPost.at<float>(0,0);
+      float rad_y = filter_ptr->errorCovPost.at<float>(1,1);
       cv::circle(img, mean, 5, cv::Scalar(255,0,0));
       cv::ellipse(img, mean, cv::Size(rad_x, rad_y), 0, 0, 360, cv::Scalar(0,255,0));
     }
 
+    printFilterState();
+
     cv::imshow("Filter States", img);
     cv::waitKey(10);
+  }
+
+  void printFilterState() {
+    for (auto filter_ptr : filter_list_) {
+      std::cout << "State: " << filter_ptr->statePost << std::endl;
+      std::cout << "Cov Post: " << filter_ptr->errorCovPost << std::endl;
+      std::cout << "Cov Pre: " << filter_ptr->errorCovPre << std::endl;
+    }
   }
 
 };
