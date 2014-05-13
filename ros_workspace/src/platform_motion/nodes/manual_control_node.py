f435d1ddd12f6b374447935bdc1a79d5c9761a5c
Jascha <jtlittle@gmail.com>
2014-05-12 17:30:04 -0700

    Added samplereturn_msgs to imports

--- ros_workspace/src/platform_motion/nodes/manual_control_node.py
+++ ros_workspace/src/platform_motion/nodes/manual_control_node.py
@@ -16,6 +16,7 @@ import platform_motion_msgs.srv as platform_srv
 import visual_servo_msgs.msg as visual_servo_msg
 import geometry_msgs.msg as geometry_msg
 import sensor_msgs.msg as sensor_msg
+import samplereturn_msgs.msg as samplereturn_msg
 import samplereturn.util as util
 
 from samplereturn_msgs.msg import VoiceAnnouncement
