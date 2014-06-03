51aef4a123e09c1f65d795972ca4cc693aed63d4
Jascha <jtlittle@gmail.com>
2014-06-02 20:28:02 -0700

    Lights.py now handles search lights

--- ros_workspace/src/samplereturn/nodes/lights.py
+++ ros_workspace/src/samplereturn/nodes/lights.py
@@ -12,10 +12,16 @@ class LightController(object):
     _all_off = 7 #bit states for all lights off
      
     def __init__(self):
-        rospy.Subscriber("pause_state", Bool, self.pause_state_update)
+        rospy.Subscriber("pause_state", Bool, self.handle_pause_state)
+        rospy.Subscriber("search_lights", Bool, self.handle_search_lights)
         self.pub = rospy.Publisher('gpio_write', GPIO)
         self.blink = False
         self.light_state = self._all_on
+        while not rospy.is_shutdown():
+            if self.pub.get_num_connections > 0:
+                break
+            rospy.sleep(0.1)
+        self.search_lights(False)
         self.cycle = rospy.Timer(rospy.Duration(1.0), self.set_lights)
 
     def set_lights(self, event): #rospy.Timer passes in an event object to this cb
@@ -26,8 +32,18 @@ class LightController(object):
             self.light_state = self._all_on
         self.pub.publish(GPIO(servo_id=1, pin_mask=7, new_pin_states=self.light_state))
         
-    def pause_state_update(self, pause_state_msg):
-        self.blink = not pause_state_msg.data
+    def handle_pause_state(self, msg):
+        self.blink = not msg.data
+
+    def handle_search_lights(self, msg):
+        self.search_lights(msg.data)
+        
+    def search_lights(self, on):
+        mask=8
+        pin_states = 0 if on else mask
+        self.pub.publish(GPIO(servo_id = 1,
+                         pin_mask = mask,
+                         new_pin_states = pin_states))
         
 def main():
     rospy.init_node('lights')
