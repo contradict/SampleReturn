0956c94342f0859544cc68a1ef265b65b3864067
zac <zachary.lizer@gmail.com>
2014-06-11 08:37:04 -0400

    Drop blue and black objects from search detector

--- ros_workspace/src/saliency_detector/src/saliency_detector.cpp
+++ ros_workspace/src/saliency_detector/src/saliency_detector.cpp
@@ -175,7 +175,8 @@ class SaliencyDetectorNode
       if (cam_model_.initialized()
           && dominant_color != "green" && dominant_color != "brown"
           && dominant_exterior_color == "green"
-          && dominant_color != "gray") {
+          && dominant_color != "gray" && dominant_color != "black"
+          && dominant_color != "blue") {
         //float scale = cv_ptr->image.rows/600.;
         float scale = cv_ptr->image.cols/bms_img_width_;
         cv::Point3d ray =
