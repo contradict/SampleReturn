74ba425ead7c60706dbab7fe71e3ce9d52b8d56e
contradict <contradict@gmail.com>
2015-02-01 10:00:31 -0800

    Add special case for startup
    Wait for the first image to arrive before beginning regular trigger

--- ros_workspace/src/samplereturn/nodes/triggered_camera.py
+++ ros_workspace/src/samplereturn/nodes/triggered_camera.py
@@ -2,8 +2,21 @@
 """
 Use GPIO to trigger a camera at a constant rate.
 
+Simple description of strategy:
 Listen to the resulting image topic and attach GPIO timestamps to the
 corresponding images.
+
+Timeuts:
+    No image received in error_timeut - clear queue, wait for restart_delay
+        with no images, the restart capture
+    After sending first trigger - wait for first image
+        If not received within error_timeout, clear queue, send another trigger
+        and restart waiting for first image
+        When image is received, stop first image timer and start regular
+        triggering
+
+If queue gets deeper than queue_size_warning, stop triggering, clear queue, wait
+restart_delay with no images, then restart first trigger.
 """
 import rospy
 import yaml
@@ -62,25 +75,37 @@ class TriggeredCamera(object):
                 queue_size=2, buff_size=2*5000*4000)
         rospy.Subscriber('pause_state', std_msg.Bool, self.handle_pause)
 
-        self.error_status_timer = None
+        self.missing_image_timer = None
+        self.pause_for_restart_timer = None
+        self.wait_for_first_image_timer = None
+        self.trigger_timer = None
         self.start_trigger_timer(None)
 
     def handle_pause(self, msg):
         self.paused = msg.data
 
     def handle_image(self, msg):
-        if self.error_status_timer is not None:
-            self.error_status_timer.shutdown()
-            self.error_status_timer = None
+        if self.missing_image_timer is not None:
+            self.missing_image_timer.shutdown()
+            self.missing_image_timer = None
+
+        if self.wait_for_first_image_timer is not None:
+            self.wait_for_first_image_timer.shutdown()
+            self.wait_for_first_image_timer = None
+            rospy.loginfo("Got first image, beginning regular trigger.")
+            self.trigger_timer = rospy.Timer(
+                    rospy.Duration(1./self.trigger_rate),
+                    self.trigger_camera)
+
+        if self.pause_for_restart_timer is not None:
+            self.start_restart_timer()
+
         self.image_count += 1
         rospy.logdebug("image seq: %d queue: %d", self.image_count,
                 self.timestamp_queue.qsize())
         try:
             seq, stamp = self.timestamp_queue.get_nowait()
         except Queue.Empty:
-            if self.pause_for_restart is not None:
-                self.pause_for_restart.shutdown()
-                self.start_restart_timer()
             rospy.logerr("Received image without sending a trigger")
             return
         self.status_pub.publish("Ready")
@@ -97,32 +122,37 @@ class TriggeredCamera(object):
             rospy.logwarn("trigger count: %d, image count: %d, queue size %d",
                     self.trigger_count, self.image_count, qsz)
             self.trigger_timer.shutdown()
+            self.clear_queue("Queue too deep, limit %d"%self.queue_size_warning)
             self.start_restart_timer()
 
     def start_restart_timer(self):
-        self.clear_queue("Queue too deep, limit %d"%self.queue_size_warning)
-        self.pause_for_restart = rospy.Timer(
+        if self.pause_for_restart_timer is not None:
+            self.pause_for_restart_timer.shutdown()
+        self.pause_for_restart_timer = rospy.Timer(
                 rospy.Duration(self.restart_delay),
                 self.start_trigger_timer,
                 oneshot=True)
 
     def start_trigger_timer(self, evt):
+        if self.pause_for_restart_timer is not None:
+            self.pause_for_restart_timer.shutdown()
+            self.pause_for_restart_timer = None
         rospy.loginfo("starting trigger")
         self.photo_config(SetConfigRequest(param="recordingmedia",
             value="1 SDRAM"))
-        self.pause_for_restart = None
-        self.trigger_timer = rospy.Timer(rospy.Duration(1./self.trigger_rate),
-                self.trigger_camera)
+        self.send_one_trigger()
+        self.wait_for_first_image_timer = rospy.Timer(
+                rospy.Duration(2*self.error_timeout),
+                self.no_first_image,
+                oneshot=True)
 
-    def trigger_camera(self, evt):
-        if self.paused:
-            if self.error_status_timer is not None:
-                self.error_status_timer.shutdown()
-                self.error_status_timer = None
-            return
+    def no_first_image(self, evt):
+        self.clear_queue("First trigger sent, no image received, retrying.")
+        self.wait_for_first_image_timer = None
+        self.start_trigger_timer(None)
+
+    def send_one_trigger(self):
         self.trigger_count += 1
-        rospy.logdebug("trigger seq: %d queue: %d", self.trigger_count,
-                self.timestamp_queue.qsize())
         gpio_req = GPIOServiceRequest(GPIO(servo_id=self.gpio_servo_id,
                                    new_pin_states=0,
                                    pin_mask=self.gpio_pin))
@@ -132,8 +162,18 @@ class TriggeredCamera(object):
         self.gpio_service(gpio_req)
         capture_stamp += rospy.Duration(self.time_offset)
         self.timestamp_queue.put((self.trigger_count, capture_stamp))
-        if self.error_status_timer is None:
-            self.error_status_timer = rospy.Timer(
+
+    def trigger_camera(self, evt):
+        if self.paused:
+            if self.missing_image_timer is not None:
+                self.missing_image_timer.shutdown()
+                self.missing_image_timer = None
+            return
+        self.send_one_trigger()
+        rospy.logdebug("trigger seq: %d queue: %d", self.trigger_count,
+                self.timestamp_queue.qsize())
+        if self.missing_image_timer is None:
+            self.missing_image_timer = rospy.Timer(
                     rospy.Duration(self.error_timeout),
                     lambda evt: self.clear_queue("Image receipt timeout."),
                     oneshot=True)
