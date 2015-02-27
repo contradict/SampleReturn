c4c95b10256dcc77401ae2701b3b3a8b7e0ab260
contradict <contradict@gmail.com>
2015-02-26 20:02:12 -0800

    Add stereo FOV calculations

--- src/scripts/fov.py
+++ src/scripts/fov.py
@@ -25,19 +25,50 @@ def compute_fov(cam_info):
     bottom = np.arctan((cam_info.height-K[1, 2])/K[1, 1])
     return left, right, top, bottom
 
+def compute_stereo_fov(left_info, right_info, max_d, window):
+    Kleft = np.array(left_info.K).reshape((3, 3))
+    fx = Kleft[0, 0]
+    fy = Kleft[1, 1]
+    cx = Kleft[0, 2]
+    cy = Kleft[1, 2]
+    Pright = np.array(right_info.P).reshape((3, 4))
+    Tx = -Pright[0, 3]
+    left = np.arctan((cx-window/2.-max_d)/fx)
+    right = np.arctan((left_info.width - window/2. - cx)/fx)
+    top = np.arctan((Kleft[1, 2]-window/2.)/fy)
+    bottom = np.arctan((left_info.height-cy-window/2.)/fy)
+    z_min = Tx/max_d
+    z_max = Tx/1.0
+    return left, right, top, bottom, z_min, z_max
+
 def main():
     if len(sys.argv)<2:
         print("%s calibration_file"%sys.argv[0])
         print("prints field of view for calibrated camera")
         exit(1)
-    inf = parse_yaml(sys.argv[1])
-    l,r,t,b = compute_fov(inf)
+    if len(sys.argv) == 2:
+        inf = parse_yaml(sys.argv[1])
+        l, r, t, b = compute_fov(inf)
+    else:
+        left = parse_yaml(sys.argv[1])
+        right = parse_yaml(sys.argv[2])
+        max_d = 48
+        if len(sys.argv)>3:
+            max_d = int(sys.argv[3])
+        win_sz = 21
+        if len(sys.argv)>4:
+            win_sz = int(sys.argv[4])
+        l, r, t, b, z_min, z_max = compute_stereo_fov(left, right,
+                max_d, win_sz)
+        print("Z range (%4.2f, %4.2f)"%(z_min, z_max))
     print("Horizontal: %4.1f"%np.degrees(l+r))
     print("Vertical:   %4.1f"%np.degrees(t+b))
     print("Left, right of center: (%4.1f, %4.1f)"%
             (np.degrees(l), np.degrees(r)))
     print("Above, below center:   (%4.1f, %4.1f)"%
             (np.degrees(t), np.degrees(b)))
+
+
 
 if __name__ == "__main__":
     main()
