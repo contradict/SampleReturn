c029343eded56c359ba39d223d0ff9d1440d5966
zac <zachary.lizer@gmail.com>
2015-04-16 16:59:09 -0700

    Dynamic reconfigure, trim the ground, publish marker

--- ros_workspace/src/fence_detection/nodes/simple_fence_detection.cpp
+++ ros_workspace/src/fence_detection/nodes/simple_fence_detection.cpp
@@ -6,6 +6,8 @@
 #include <stereo_msgs/DisparityImage.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
+#include <visualization_msgs/Marker.h>
+#include <tf/transform_listener.h>
 
 #include <dynamic_reconfigure/server.h>
 #include <fence_detection/fence_detector_paramsConfig.h>
@@ -16,11 +18,15 @@
 
 #include <pcl/point_types.h>
 #include <pcl/conversions.h>
+#include <pcl/filters/impl/plane_clipper3D.hpp>
+#include <pcl/filters/extract_indices.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_ros/transforms.h>
+#include <eigen_conversions/eigen_msg.h>
 
 class FenceDetectorNode
 {
@@ -31,6 +37,8 @@ class FenceDetectorNode
   ros::Subscriber r_camera_info_sub;
   ros::Publisher mask_pub;
   ros::Publisher points_pub;
+  ros::Publisher marker_pub;
+  ros::Publisher line_marker_pub;
 
   std::string color_img_topic = "color_image";
   std::string disparity_img_topic = "disparity_image";
@@ -38,8 +46,11 @@ class FenceDetectorNode
   std::string r_camera_info_topic = "right/camera_info";
   std::string mask_pub_topic = "fence_mask";
   std::string points_pub_topic = "points";
+  std::string marker_pub_topic = "plane_marker";
+  std::string line_marker_pub_topic = "line_marker";
 
-  double a_channel_threshold_, x_edge_threshold_, y_edge_threshold_, sum_threshold_;
+  double a_channel_threshold_, b_channel_threshold_, x_edge_threshold_, y_edge_threshold_, sum_threshold_;
+  double max_range_, min_height_;
   int dilate_iterations_, erode_iterations_;
 
   sensor_msgs::CameraInfo r_cam_info_;
@@ -48,6 +59,8 @@ class FenceDetectorNode
   stereo_msgs::DisparityImage disp_msg_;
 
   dynamic_reconfigure::Server<fence_detection::fence_detector_paramsConfig> dr_srv;
+
+  tf::TransformListener listener_;
 
   public:
   FenceDetectorNode() {
@@ -59,6 +72,7 @@ class FenceDetectorNode
 
     ros::NodeHandle private_node_handle_("~");
     private_node_handle_.param("a_channel_threshold",a_channel_threshold_,double(170));
+    private_node_handle_.param("b_channel_threshold",b_channel_threshold_,double(170));
     private_node_handle_.param("x_edge_threshold",x_edge_threshold_,double(150));
     private_node_handle_.param("y_edge_threshold",y_edge_threshold_,double(150));
     private_node_handle_.param("sum_threshold",sum_threshold_,double(85));
@@ -82,6 +96,12 @@ class FenceDetectorNode
 
     points_pub =
       nh.advertise<sensor_msgs::PointCloud2>(points_pub_topic.c_str(), 3);
+
+    marker_pub =
+      nh.advertise<visualization_msgs::Marker>(marker_pub_topic.c_str(), 3);
+
+    line_marker_pub =
+      nh.advertise<visualization_msgs::Marker>(line_marker_pub_topic.c_str(), 3);
 
   }
 
@@ -119,11 +139,13 @@ class FenceDetectorNode
     cv::Mat lab;
     cv::cvtColor(cv_ptr->image, lab, CV_BGR2Lab);
     cv::Mat a(lab.rows,lab.cols,CV_8UC1);
+    cv::Mat b(lab.rows,lab.cols,CV_8UC1);
     cv::Mat x_edge, y_edge;
-    cv::Mat x_thresh, y_thresh, a_thresh;
-    cv::Mat x_mask, y_mask, a_mask, sum_mask;
-    int from_to[] = { 1,0 };
-    cv::mixChannels( &lab, 1, &a, 1, from_to, 1);
+    cv::Mat x_thresh, y_thresh, a_thresh, b_thresh;
+    cv::Mat x_mask, y_mask, a_mask, b_mask, sum_mask;
+    int from_to[] = { 1,0 , 2,0};
+    cv::Mat out[] = {a,b};
+    cv::mixChannels( &lab, 1, out, 1, from_to, 1);
     cv::Sobel(a, x_edge, CV_32F, 1, 0, 3, 1, 128);
     cv::Sobel(a, y_edge, CV_32F, 0, 1, 3, 1, 128);
     x_edge.convertTo(x_edge,CV_8U);
@@ -131,25 +153,29 @@ class FenceDetectorNode
     cv::threshold(x_edge, x_thresh, x_edge_threshold_, 255, cv::THRESH_BINARY);
     cv::threshold(y_edge, y_thresh, y_edge_threshold_, 255, cv::THRESH_BINARY);
     cv::threshold(a, a_thresh, a_channel_threshold_, 255, cv::THRESH_BINARY);
+    cv::threshold(b, b_thresh, b_channel_threshold_, 255, cv::THRESH_BINARY);
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(3,3));
     cv::dilate(x_thresh, x_mask, kernel, cv::Point(-1,-1), dilate_iterations_);
     cv::dilate(y_thresh, y_mask, kernel, cv::Point(-1,-1), dilate_iterations_);
     cv::dilate(a_thresh, a_mask, kernel, cv::Point(-1,-1), dilate_iterations_);
+    cv::dilate(b_thresh, b_mask, kernel, cv::Point(-1,-1), dilate_iterations_);
     cv::erode(x_mask, x_mask, kernel, cv::Point(-1,-1), erode_iterations_);
     cv::erode(y_mask, y_mask, kernel, cv::Point(-1,-1), erode_iterations_);
     cv::erode(a_mask, a_mask, kernel, cv::Point(-1,-1), erode_iterations_);
+    cv::erode(b_mask, b_mask, kernel, cv::Point(-1,-1), erode_iterations_);
 
     x_mask.convertTo(x_mask,CV_32F);
     y_mask.convertTo(y_mask,CV_32F);
     a_mask.convertTo(a_mask,CV_32F);
+    b_mask.convertTo(b_mask,CV_32F);
 
-    cv::threshold((x_mask+y_mask+a_mask)/float(3), sum_mask, sum_threshold_, 255,
+    cv::threshold((x_mask+y_mask+a_mask+b_mask)/float(4), sum_mask, sum_threshold_, 255,
         cv::THRESH_BINARY);
 
     sum_mask.convertTo(sum_mask,CV_8U);
 
     std_msgs::Header header;
-    sensor_msgs::ImagePtr debug_img_msg = cv_bridge::CvImage(header,"mono8",x_thresh).toImageMsg();
+    sensor_msgs::ImagePtr debug_img_msg = cv_bridge::CvImage(header,"mono8",sum_mask).toImageMsg();
     mask_pub.publish(debug_img_msg);
 
     sensor_msgs::PointCloud2Ptr points_msg = boost::make_shared<sensor_msgs::PointCloud2>();
@@ -182,7 +208,39 @@ class FenceDetectorNode
         }
       }
     }
-    points_pub.publish(points_msg);
+    //points_pub.publish(points_msg);
+
+    /* Transform points to base_link frame */
+    sensor_msgs::PointCloud2 base_link_points_msg;
+    pcl_ros::transformPointCloud("/base_link",*points_msg,base_link_points_msg,listener_);
+
+    pcl::PCLPointCloud2 pcl_pc;
+    pcl_conversions::toPCL(base_link_points_msg, pcl_pc);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr ptr_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr clipped_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr far_clipped_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(pcl_pc,*ptr_cloud);
+
+    /* Trim ground points */
+    Eigen::Vector4f ground_plane;
+    ground_plane.setZero();
+    ground_plane[2] = 1.0;
+    ground_plane[3] = -min_height_;
+    pcl::PlaneClipper3D<pcl::PointXYZ> ground_clipper(ground_plane);
+    //pcl::PointIndices::Ptr clipped (new pcl::PointIndices);
+    //std::vector<int> clipped;
+    pcl::PointIndices::Ptr clipped (new pcl::PointIndices);
+    std::vector<int> indices;
+    ground_clipper.clipPointCloud3D(*ptr_cloud,clipped->indices,indices);
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    extract.setInputCloud (ptr_cloud);
+    extract.setIndices (clipped);
+    extract.setNegative (false);
+    extract.filter (*clipped_cloud);
+
+    pcl::toPCLPointCloud2(*clipped_cloud,pcl_pc);
+    pcl_conversions::fromPCL(pcl_pc,base_link_points_msg);
+    points_pub.publish(base_link_points_msg);
 
     /* Time to fit the points */
     pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
@@ -192,14 +250,10 @@ class FenceDetectorNode
     seg.setModelType (pcl::SACMODEL_PLANE);
     seg.setDistanceThreshold (0.1);
 
-    pcl::PCLPointCloud2 pcl_pc;
-    pcl_conversions::toPCL(*points_msg, pcl_pc);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr ptr_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromPCLPointCloud2(pcl_pc,*ptr_cloud);
-
     ROS_INFO("Width, Height:%u, %u",ptr_cloud->width,ptr_cloud->height);
 
-    seg.setInputCloud(ptr_cloud);
+    seg.setInputCloud(clipped_cloud);
+    //seg.setInputCloud(ptr_cloud);
     seg.segment (*inliers, *coefficients);
     if (inliers->indices.size() == 0) {
       ROS_ERROR("Could not estimate a planar model for given dataset.");
@@ -209,6 +263,51 @@ class FenceDetectorNode
                                                   coefficients->values[1],
                                                   coefficients->values[2],
                                                   coefficients->values[3]);
+    Eigen::Vector3d centroid;
+    centroid.setZero();
+    //for(size_t i=0; i<inliers->indices.size(); i++) {
+    //  centroid += inliers->points[
+    visualization_msgs::Marker mark;
+    mark.header = msg.header;
+    mark.header.frame_id = "/base_link";
+    Eigen::Quaterniond orientation;
+    Eigen::Vector3d xhat;
+    xhat.setZero();
+    xhat[0] = 1.0;
+    Eigen::Vector3d plane_normal;
+    plane_normal << coefficients->values[0],
+                 coefficients->values[1],
+                 coefficients->values[2];
+    orientation.setFromTwoVectors(xhat, plane_normal);
+    tf::quaternionEigenToMsg(orientation, mark.pose.orientation);
+    mark.scale.x = 1.0;
+    mark.scale.y = 0.1;
+    mark.scale.z = 0.1;
+    mark.color.a = 1.0;
+    mark.color.r = 0.0;
+    mark.color.g = 0.5;
+    mark.color.b = 1.0;
+    marker_pub.publish(mark);
+
+    visualization_msgs::Marker line_mark;
+    line_mark.header = msg.header;
+    line_mark.header.frame_id = "/base_link";
+    line_mark.color.a = 1.0;
+    line_mark.color.r = 1.0;
+    line_mark.color.g = 0.4;
+    line_mark.color.b = 0.0;
+    line_mark.scale.x = 0.4;
+    geometry_msgs::Point p;
+    line_mark.pose.position.x = 2.0* -coefficients->values[3]/coefficients->values[0];
+    p.z = 0.0;
+    p.y = -5.0;
+    p.x = (coefficients->values[3] - coefficients->values[1]*p.y)/coefficients->values[0];
+    line_mark.points.push_back(p);
+    p.y = 5.0;
+    p.x = (coefficients->values[3] - coefficients->values[1]*p.y)/coefficients->values[0];
+    line_mark.points.push_back(p);
+    line_marker_pub.publish(line_mark);
+
   }
 
   /* Dynamic reconfigure callback */
@@ -218,9 +317,12 @@ class FenceDetectorNode
     x_edge_threshold_ = config.x_edge_threshold;
     y_edge_threshold_ = config.y_edge_threshold;
     a_channel_threshold_ = config.a_channel_threshold;
+    b_channel_threshold_ = config.b_channel_threshold;
     sum_threshold_ = config.sum_threshold;
     dilate_iterations_ = config.dilate_iterations;
     erode_iterations_ = config.erode_iterations;
+    min_height_ = config.min_height;
+    max_range_ = config.max_range;
   }
 
 };
