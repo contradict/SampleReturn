e9e25c388eb902120693a09fc73b5163987c1e45
zac <zachary.lizer@gmail.com>
2015-05-09 09:27:56 -0700

    Close range shape-based target verification

---
+++ ros_workspace/src/saliency_detector/nodes/close_range_detector.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import roslib
+import rospy
+import rospkg
+import numpy as np
+import cv2
+from copy import deepcopy
+
+from cv_bridge import CvBridge, CvBridgeError
+from tf.transformations import euler_from_quaternion,TransformListener
+from dynamic_reconfigure.server import Server
+
+from image_geometry import PinholeCameraModel
+from sensor_msgs.msg import Image, CameraInfo
+from samplereturn_msgs.msg import NamedPoint, Verify, VerifyResponse
+
+# Subscribe to dslr image and current pursuit target
+# When asked, take target location and get it in img coords
+# Extract window, find contour, compute shape and classify
+# Emit yes or no
+
+class CloseRangeDetector(object):
+
+    def __init__(self):
+        self.dslr_sub = rospy.Subscriber('image', Image, self.image_callback, None, 1)
+        self.dslr_info_sub = rospy.Subscriber('cam_info', CameraInfo, self.cam_info_callback, None, 1)
+        self.pursuit_sub = rospy.Subscriber('target_point', NamedPoint, self.target_point_callback, None, 1)
+
+        rospy.Service("close_range_confirm",Verify,self.verify_callback)
+
+        self.bridge = CvBridge()
+        self.tf = TransformListener()
+        self.cam_model = None
+        self.img_point = None
+        self.target_point = None
+        self.last_img = None
+
+        rospack = rospkg.RosPack()
+
+        self.classifier = cv2.SVM()
+        self.classifier.load(rospack.get_path('saliency_detector')+'/config/trained_svm'))
+
+    def verify_callback(self, req):
+        if self.img_point is not None and self.last_img is not None:
+            win = self.extract_window(self.last_img)
+            shape = self.compute_shape_metrics
+            ret = self.classifier.predict(shape)
+            resp = VerifyResponse(ret)
+            return resp
+        else:
+            rospy.logerror("Verification Request with no image or target point")
+            resp = VerifyResponse(False)
+            return
+
+    def cam_info_callback(self, CameraInfo):
+        self.cam_model = CameraInfo()
+        self.cam_model.fromCameraInfo(CameraInfo)
+
+    def target_point_callback(self, NamedPoint):
+        self.target_point = deepcopy(NamedPoint)
+
+    def image_callback(self, Image):
+        # Transform pursuit target from tracking frame to camera xyz,
+        # then project into image coords (u,v)
+        self.target_point.header.stamp = Image.header.stamp
+        search_camera_point = self.tf.transformPoint("search_camera",self.target_point)
+        if self.cam_model is not None:
+            self.img_point = self.cam_model.project3dToPixel(search_camera_point)
+
+        img = np.asarray(self.bridge.imgmsg_to_cv(Image,'rgb8'))
+        lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+        self.last_img = lab.copy()
+
+    def extract_window(self, image);
+        # Take the image, grab the widow around the img_point, handling borders
+        win = image[self.img_point[]-self.win_size:self.img_point[]+self.win_size,\
+                self.img_point[]-self.win_size:self.img_point[]+self.win_size]
+
+    def compute_shape_metrics(self, img):
+        mid = (float(np.max(img))+float(np.min(img)))/2.
+        thresh = cv2.threshold(img,mid,255,cv2.THRESH_BINARY)[1]
+        contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        largest_area = 0
+        largest_contour = 0
+        for i in contours:
+            area = cv2.contourArea(i)
+            if area > largest_area:
+                largest_area = area
+                largest_contour = i
+        hull = cv2.convexHull(largest_contour,returnPoints=False)
+        hull_pts = cv2.convexHull(largest_contour)
+        perimeter_ratio = cv2.arcLength(hull_pts,True)/cv2.arcLength(largest_contour,True)
+        area_ratio = cv2.contourArea(largest_contour)/cv2.contourArea(hull_pts)
+        defects = cv2.convexityDefects(largest_contour,hull)
+        depth = np.max(defects[...,-1])/256.0
+        rect = cv2.boundingRect(hull_pts)
+        defect_ratio = depth/np.max(rect[2:])
+        return np.array([perimeter_ratio,area_ratio,defect_ratio])
+
+if __name__=="__main__":
+  try:
+    CloseRangeDetector()
+    rospy.spin()
+  except rospy.ROSInterruptException: pass
