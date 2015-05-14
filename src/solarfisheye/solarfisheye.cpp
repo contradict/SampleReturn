4a52229b4df574e9dfc7d318308417a663ab754e
James Sarrett <jsarrett@gmail.com>
2015-05-13 21:27:27 -0700

    fix missing include in test code

--- src/solarfisheye/solarfisheye.cpp
+++ src/solarfisheye/solarfisheye.cpp
@@ -6,6 +6,7 @@
 
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/core/affine.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <stdio.h>
