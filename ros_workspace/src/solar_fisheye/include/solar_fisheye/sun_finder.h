860df2c28bf353d55f785d5e167d31d61815e94d
James Sarrett <jsarrett@gmail.com>
2015-05-16 14:03:15 -0700

    consume image and compute moments, printing a test vector to stdout

--- ros_workspace/src/solar_fisheye/include/solar_fisheye/sun_finder.h
+++ ros_workspace/src/solar_fisheye/include/solar_fisheye/sun_finder.h
@@ -3,16 +3,26 @@
 #include <image_transport/camera_subscriber.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
+#include <image_geometry/pinhole_camera_model.h>
 #include <cv_bridge/cv_bridge.h>
 #include <dynamic_reconfigure/server.h>
+
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/affine.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <solar_fisheye/SolarFisheyeConfig.h>
 
 namespace solar_fisheye {
+
+void unprojectPointsFisheye( cv::InputArray distorted, cv::OutputArray undistorted, cv::InputArray K, cv::InputArray D, cv::InputArray R, cv::InputArray P);
+
 class SunFinder
 {
     image_transport::ImageTransport *it_;
     image_transport::CameraSubscriber sub_;
+    image_geometry::PinholeCameraModel model_;
+
 
     SolarFisheyeConfig config_;
     dynamic_reconfigure::Server<solar_fisheye::SolarFisheyeConfig> reconfigure_server_;
--- ros_workspace/src/solar_fisheye/src/sun_finder.cpp
+++ ros_workspace/src/solar_fisheye/src/sun_finder.cpp
@@ -4,6 +4,98 @@
 
 namespace solar_fisheye {
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// taken from cv::fisheye::undistortPoints
+
+void unprojectPointsFisheye( cv::InputArray distorted, cv::OutputArray undistorted, cv::InputArray K, cv::InputArray D, cv::InputArray R, cv::InputArray P)
+{
+    // will support only 2-channel data now for points
+    CV_Assert(distorted.type() == CV_32FC2 || distorted.type() == CV_64FC2);
+    undistorted.create(distorted.size(), CV_MAKETYPE(distorted.depth(), 3));
+
+    CV_Assert(P.empty() || P.size() == cv::Size(3, 3) || P.size() == cv::Size(4, 3));
+    CV_Assert(R.empty() || R.size() == cv::Size(3, 3) || R.total() * R.channels() == 3);
+    CV_Assert(D.total() == 4 && K.size() == cv::Size(3, 3) && (K.depth() == CV_32F || K.depth() == CV_64F));
+
+    cv::Vec2d f, c;
+    if (K.depth() == CV_32F)
+    {
+        cv::Matx33f camMat = K.getMat();
+        f = cv::Vec2f(camMat(0, 0), camMat(1, 1));
+        c = cv::Vec2f(camMat(0, 2), camMat(1, 2));
+    }
+    else
+    {
+        cv::Matx33d camMat = K.getMat();
+        f = cv::Vec2d(camMat(0, 0), camMat(1, 1));
+        c = cv::Vec2d(camMat(0, 2), camMat(1, 2));
+    }
+
+    cv::Vec4d k = D.depth() == CV_32F ? (cv::Vec4d)*D.getMat().ptr<cv::Vec4f>(): *D.getMat().ptr<cv::Vec4d>();
+
+    cv::Matx33d RR = cv::Matx33d::eye();
+    if (!R.empty() && R.total() * R.channels() == 3)
+    {
+        cv::Vec3d rvec;
+        R.getMat().convertTo(rvec, CV_64F);
+        RR = cv::Affine3d(rvec).rotation();
+    }
+    else if (!R.empty() && R.size() == cv::Size(3, 3))
+        R.getMat().convertTo(RR, CV_64F);
+
+    if(!P.empty())
+    {
+        cv::Matx33d PP;
+        P.getMat().colRange(0, 3).convertTo(PP, CV_64F);
+        RR = PP * RR;
+    }
+
+    // start undistorting
+    const cv::Vec2f* srcf = distorted.getMat().ptr<cv::Vec2f>();
+    const cv::Vec2d* srcd = distorted.getMat().ptr<cv::Vec2d>();
+    cv::Vec3f* dstf = undistorted.getMat().ptr<cv::Vec3f>();
+    cv::Vec3d* dstd = undistorted.getMat().ptr<cv::Vec3d>();
+
+    size_t n = distorted.total();
+    int sdepth = distorted.depth();
+
+    for(size_t i = 0; i < n; i++ )
+    {
+        cv::Vec2d pi = sdepth == CV_32F ? (cv::Vec2d)srcf[i] : srcd[i];  // image point
+        cv::Vec2d pw((pi[0] - c[0])/f[0], (pi[1] - c[1])/f[1]);      // world point
+
+        double scale = 1.0;
+
+        double theta_d = sqrt(pw[0]*pw[0] + pw[1]*pw[1]);
+        if (theta_d > 1e-8)
+        {
+            // compensate distortion iteratively
+            double theta = theta_d;
+            for(int j = 0; j < 10; j++ )
+            {
+                double theta2 = theta*theta, theta4 = theta2*theta2, theta6 = theta4*theta2, theta8 = theta6*theta2;
+                theta = theta_d / (1 + k[0] * theta2 + k[1] * theta4 + k[2] * theta6 + k[3] * theta8);
+            }
+
+            //scale = std::tan(theta) / theta_d;
+            scale = theta / theta_d;
+        }
+
+        cv::Vec2d pu = pw * scale; //undistorted point
+
+        // reproject
+        cv::Vec3d pr = RR * cv::Vec3d(pu[0], pu[1], 1.0); // rotated point optionally multiplied by new camera matrix
+        //Vec2d fi(pr[0]/pr[2], pr[1]/pr[2]);       // final
+        cv::Vec3d fi;       // final
+        normalize(pr, fi);
+
+        if( sdepth == CV_32F )
+            dstf[i] = fi;
+        else
+            dstd[i] = fi;
+    }
+}
+
 SunFinder::SunFinder()
 {
     ros::NodeHandle nh("~");
@@ -26,7 +118,26 @@ SunFinder::imageCallback(const sensor_msgs::ImageConstPtr &image_msg,
 {
     ROS_DEBUG("Got an image");
     cv::Mat imageMat = cv_bridge::toCvShare(image_msg, "mono8")->image;
+    model_.fromCameraInfo(info_msg);
+
     // do cool stuff with imageMat and info_msg
+    cv::Mat img_thr = cv::Mat::zeros( imageMat.size(), CV_8UC1 );
+    cv::threshold(img_thr, imageMat, config_.noise_threshold, 255, cv::THRESH_TOZERO);
+
+    /// Get the moments
+    cv::Moments mu = cv::moments(img_thr, false );
+    ///  Get the mass center:
+    cv::Point2f mc = cv::Point2f( static_cast<float>(mu.m10/mu.m00) , static_cast<float>(mu.m01/mu.m00) );
+
+    std::vector<cv::Point2f> pts;
+    pts.push_back(mc);
+    std::vector<cv::Point3f> undist_pts;
+
+    unprojectPointsFisheye(pts, undist_pts, model_.fullIntrinsicMatrix(), model_.distortionCoeffs(), cv::Mat(), cv::Mat());
+    std::cout << pts << std::endl;
+    std::cout << undist_pts << std::endl;
+    printf("intensity: %i\n", img_thr.at<uint8_t>(mc));
+    //cout << src_thr.at<uint8_t>(mc) << endl;
 }
 
 }
