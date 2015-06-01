7f04c0b2f31ae16eb496ed3276c6498f34160858
contradict <contradict@gmail.com>
2015-05-31 18:34:34 -0700

    Add curvature and yaw to odometry filter

--- ros_workspace/src/platform_motion/include/odometry/EKF_odometry.h
+++ ros_workspace/src/platform_motion/include/odometry/EKF_odometry.h
@@ -1,8 +1,9 @@
 
 #include <pdf/analyticconditionalgaussian_additivenoise.h>
 #include <pdf/linearanalyticconditionalgaussian.h>
-#include <model/linearanalyticsystemmodel_gaussianuncertainty.h>
+#include <model/linearanalyticmeasurementmodel_gaussianuncertainty.h>
 #include <filter/extendedkalmanfilter.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 #include "odometry/odometry_node.h"
 #include "odometry/wheelpodmeasurementpdf.h"
@@ -18,17 +19,25 @@ class EKFOdometryNode: public OdometryNode
 
     private:
         void initializeModels(void);
+        void updateYaw(const geometry_msgs::PoseWithCovarianceStampedConstPtr& yawmsg);
 
         double sigma_system_noise_x, sigma_system_noise_y, sigma_system_noise_theta,
-               sigma_system_noise_vx, sigma_system_noise_vy, sigma_system_noise_omega;
-        double sigma_meas_noise_v;
+               sigma_system_noise_vx, sigma_system_noise_vy, sigma_system_noise_omega,
+               sigma_system_noise_curvature;
+        double sigma_wheel_meas_noise_v;
         double sigma_prior_pos, sigma_prior_theta,
-               sigma_prior_vel, sigma_prior_omega;
+               sigma_prior_vel, sigma_prior_omega,
+               sigma_prior_curvature;
+        double max_yaw_correction_rate;
 
-        BFL::LinearAnalyticConditionalGaussian *sys_pdf;
-        BFL::LinearAnalyticSystemModelGaussianUncertainty *sys_model;
-        BFL::AnalyticConditionalGaussian *meas_pdf;
-        BFL::AnalyticMeasurementModelGaussianUncertainty *meas_model;
+        ros::Subscriber yaw_sub;
+
+        BFL::AnalyticConditionalGaussianAdditiveNoise *sys_pdf;
+        BFL::AnalyticSystemModelGaussianUncertainty *sys_model;
+        BFL::AnalyticConditionalGaussian *wheel_meas_pdf;
+        BFL::AnalyticMeasurementModelGaussianUncertainty *wheel_meas_model;
+        BFL::LinearAnalyticConditionalGaussian *sun_meas_pdf;
+        BFL::LinearAnalyticMeasurementModelGaussianUncertainty *sun_meas_model;
         BFL::Gaussian *prior;
         BFL::ExtendedKalmanFilter *filter;
 
--- ros_workspace/src/platform_motion/src/EKF_odometry.cpp
+++ ros_workspace/src/platform_motion/src/EKF_odometry.cpp
@@ -1,8 +1,10 @@
 #include <pdf/analyticconditionalgaussian_additivenoise.h>
 #include <pdf/gaussian.h>
+#include <pdf/linearanalyticconditionalgaussian.h>
 #include <iostream>
 
 #include "odometry/EKF_odometry.h"
+#include "odometry/odometrywithcurvature.h"
 
 #define MEASMODEL_NUMCONDARGUMENTS_THREEWHEEL 2
 #define MEASMODEL_DIMENSION_THREEWHEEL        3
@@ -20,13 +22,19 @@ EKFOdometryNode::EKFOdometryNode(void):OdometryNode()
     param_nh.param("sigma_system_noise_vx", sigma_system_noise_vx, 0.05);
     param_nh.param("sigma_system_noise_vy", sigma_system_noise_vy, 0.05);
     param_nh.param("sigma_system_noise_omega", sigma_system_noise_omega, 0.10);
+    param_nh.param("sigma_system_noise_curvature", sigma_system_noise_curvature, 0.001);
 
-    param_nh.param("sigma_meas_noise_v", sigma_meas_noise_v, 0.002);
+    param_nh.param("sigma_wheel_meas_noise_v", sigma_wheel_meas_noise_v, 0.002);
 
     param_nh.param("sigma_prior_pos", sigma_prior_pos, 1.00);
     param_nh.param("sigma_prior_theta", sigma_prior_theta, 0.10);
     param_nh.param("sigma_prior_vel", sigma_prior_vel, 0.05);
     param_nh.param("sigma_prior_omega", sigma_prior_omega, 0.01);
+    param_nh.param("sigma_prior_curvature", sigma_prior_curvature, 0.001);
+
+    param_nh.param("max_yaw_correction_rate", max_yaw_correction_rate, 0.05);
+
+    yaw_sub = param_nh.subscribe("sun_yaw", 2, &EKFOdometryNode::updateYaw, this);
 }
 
 void EKFOdometryNode::init(void)
@@ -39,68 +47,83 @@ void EKFOdometryNode::init(void)
 void EKFOdometryNode::initializeModels(void)
 {
     /****************************
-     * Linear system model      *
+     * Nonlinear system model      *
      ***************************/
 
-    MatrixWrapper::Matrix A(3,3);
-    A(1,1) = 1.0; A(1,2) = 0.0; A(1,3) = 0.0;
-    A(2,1) = 0.0; A(2,2) = 1.0; A(2,3) = 0.0;
-    A(3,1) = 0.0; A(3,2) = 0.0; A(3,3) = 1.0;
-
-    std::vector<MatrixWrapper::Matrix> vA(1);
-    vA[0] = A;
 
     // create gaussian
-    MatrixWrapper::ColumnVector sysNoise_Mu(3);
+    MatrixWrapper::ColumnVector sysNoise_Mu(5);
     sysNoise_Mu = 0;
 
-    MatrixWrapper::SymmetricMatrix sysNoise_Cov(3);
+    MatrixWrapper::SymmetricMatrix sysNoise_Cov(5);
     sysNoise_Cov = 0.0;
     sysNoise_Cov(1,1) = sigma_system_noise_vx;
     sysNoise_Cov(2,2) = sigma_system_noise_vy;
     sysNoise_Cov(3,3) = sigma_system_noise_omega;
+    sysNoise_Cov(4,4) = sigma_system_noise_theta;
+    sysNoise_Cov(5,5) = sigma_system_noise_curvature;
 
     BFL::Gaussian system_Uncertainty(sysNoise_Mu, sysNoise_Cov);
-    sys_pdf = new BFL::LinearAnalyticConditionalGaussian(vA, system_Uncertainty);
+    sys_pdf = new platform_odometry::OdometryWithCurvature(system_Uncertainty);
     // create the system model
-    sys_model = new BFL::LinearAnalyticSystemModelGaussianUncertainty(sys_pdf);
+    sys_model = new BFL::AnalyticSystemModelGaussianUncertainty(sys_pdf);
 
-    /*********************************
-     * Initialise measurement model *
-     ********************************/
+    /****************************************
+    * Initialise odometry measurement model *
+    ****************************************/
 
-    MatrixWrapper::ColumnVector measNoise_Mu(6);
-    measNoise_Mu = 0;
-    MatrixWrapper::SymmetricMatrix measNoise_Cov(6);
-    measNoise_Cov = 0;
-    measNoise_Cov(1,1) = sigma_meas_noise_v;
-    measNoise_Cov(2,2) = sigma_meas_noise_v;
-    measNoise_Cov(3,3) = sigma_meas_noise_v;
-    measNoise_Cov(4,4) = sigma_meas_noise_v;
-    measNoise_Cov(5,5) = sigma_meas_noise_v;
-    measNoise_Cov(6,6) = sigma_meas_noise_v;
+    MatrixWrapper::ColumnVector wheelmeasNoise_Mu(6);
+    wheelmeasNoise_Mu = 0;
+    MatrixWrapper::SymmetricMatrix wheelmeasNoise_Cov(6);
+    wheelmeasNoise_Cov = 0;
+    wheelmeasNoise_Cov(1,1) = sigma_wheel_meas_noise_v;
+    wheelmeasNoise_Cov(2,2) = sigma_wheel_meas_noise_v;
+    wheelmeasNoise_Cov(3,3) = sigma_wheel_meas_noise_v;
+    wheelmeasNoise_Cov(4,4) = sigma_wheel_meas_noise_v;
+    wheelmeasNoise_Cov(5,5) = sigma_wheel_meas_noise_v;
+    wheelmeasNoise_Cov(6,6) = sigma_wheel_meas_noise_v;
 
-    BFL::Gaussian measurement_Uncertainty(measNoise_Mu, measNoise_Cov);
+    BFL::Gaussian wheel_measurement_Uncertainty(wheelmeasNoise_Mu, wheelmeasNoise_Cov);
     MatrixWrapper::Matrix pod_positions(3,3);
     pod_positions = 0;
     pod_positions(1,1) = data.port_pos[0];      pod_positions(2,1) = data.port_pos[1];
     pod_positions(1,2) = data.starboard_pos[0]; pod_positions(2,2) = data.starboard_pos[1];
     pod_positions(1,3) = data.stern_pos[0];     pod_positions(2,3) = data.stern_pos[1];
-    meas_pdf = new WheelPodMeasurementPdf(measurement_Uncertainty, pod_positions);
+    wheel_meas_pdf = new WheelPodMeasurementPdf(wheel_measurement_Uncertainty, pod_positions);
     // create the model
-    meas_model = new BFL::AnalyticMeasurementModelGaussianUncertainty(meas_pdf);
+    wheel_meas_model = new BFL::AnalyticMeasurementModelGaussianUncertainty(wheel_meas_pdf);
+
+    /***********************************
+    * Initialize sun measurement model 
+    *  set uncertianty to N(0,1) for now, This will be updated by the
+    *  covariance data from the message
+    ***********************************/
+    MatrixWrapper::ColumnVector sunNoiseMu(1);
+    sunNoiseMu = 0.0;
+    MatrixWrapper::SymmetricMatrix sunNoiseCov(1);
+    sunNoiseCov = 1.0;
+    BFL::Gaussian sunUncertainty(sunNoiseMu, sunNoiseCov);
+    MatrixWrapper::Matrix Hsun(1,5);
+    Hsun = 0.0;
+    Hsun(1,4) = 1.0;
+    std::vector<MatrixWrapper::Matrix> Hmats;
+    Hmats.push_back(Hsun);
+    sun_meas_pdf = new BFL::LinearAnalyticConditionalGaussian(Hmats, sunUncertainty);
+    sun_meas_model = new BFL::LinearAnalyticMeasurementModelGaussianUncertainty(sun_meas_pdf);
 
     /****************************
      * Linear prior DENSITY     *
      ***************************/
     // Continuous Gaussian prior (for Kalman filters)
-    MatrixWrapper::ColumnVector prior_Mu(3);
+    MatrixWrapper::ColumnVector prior_Mu(5);
     prior_Mu = 0;
-    MatrixWrapper::SymmetricMatrix prior_Cov(3);
+    MatrixWrapper::SymmetricMatrix prior_Cov(5);
     prior_Cov = 0;
     prior_Cov(1,1) = sigma_prior_vel;
     prior_Cov(2,2) = sigma_prior_vel;
     prior_Cov(3,3) = sigma_prior_omega;
+    prior_Cov(4,4) = sigma_prior_theta;
+    prior_Cov(5,5) = sigma_prior_curvature;
     prior = new BFL::Gaussian(prior_Mu,prior_Cov);
 
     /******************************
@@ -125,23 +148,19 @@ void EKFOdometryNode::computeOdometry(struct odometry_measurements &data, const 
     input(3) = data.stern_angle;
 
     // UPDATE FILTER
-    filter->Update(sys_model,meas_model,measurement,input);
+    filter->Update(sys_model,wheel_meas_model,measurement,input);
 
     BFL::Pdf<MatrixWrapper::ColumnVector> * posteriorPDF = filter->PostGet();
     MatrixWrapper::ColumnVector posterior(posteriorPDF->ExpectedValueGet());
     MatrixWrapper::SymmetricMatrix posteriorCov(posteriorPDF->CovarianceGet());
 
-
     // integrate in odometry frame
     MatrixWrapper::Matrix R(2,2);
-    R(1,1) = cos(odom_orientation); R(1,2) = -sin(odom_orientation);
-    R(2,1) = sin(odom_orientation); R(2,2) =  cos(odom_orientation);
+    R(1,1) = cos(posterior(4)); R(1,2) = -sin(posterior(4));
+    R(2,1) = sin(posterior(4)); R(2,2) =  cos(posterior(4));
     MatrixWrapper::ColumnVector OdometryFrameVelocity = R*posterior.sub(1,2);
     odom_position[0] += OdometryFrameVelocity(1);
     odom_position[1] += OdometryFrameVelocity(2);
-    odom_orientation += posterior(3);
-    while(odom_orientation > 2*M_PI) odom_orientation -= 2*M_PI;
-    while(odom_orientation < 0) odom_orientation += 2*M_PI;
     odom_velocity[0] = posterior(1)/data.interval;
     odom_velocity[1] = posterior(2)/data.interval;
     odom_omega = posterior(3)/data.interval;
@@ -164,6 +183,20 @@ void EKFOdometryNode::computeOdometry(struct odometry_measurements &data, const 
 
     resetReferencePose(data, stamp);
 
+}
+
+void EKFOdometryNode::updateYaw(const geometry_msgs::PoseWithCovarianceStampedConstPtr& yawmsg)
+{
+    MatrixWrapper::ColumnVector x=filter->PostGet()->ExpectedValueGet();
+    if(fabs(x(3))>max_yaw_correction_rate)
+        return;
+    MatrixWrapper::ColumnVector measurement(1);
+    measurement(1) = tf::getYaw(yawmsg->pose.pose.orientation);
+
+    MatrixWrapper::SymmetricMatrix sunCov(1);
+    sunCov(1,1) = yawmsg->pose.covariance[3*6+3];
+    sun_meas_pdf->AdditiveNoiseSigmaSet(sunCov);
+    filter->Update(sys_model,sun_meas_model,measurement);
 }
 
 }
