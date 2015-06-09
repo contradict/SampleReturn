2314c123a3ce8a96dfcb75586b58ba4f53b9ea54
zac <zachary.lizer@gmail.com>
2015-06-08 22:59:16 -0400

    Libraries for debugging

--- ros_workspace/src/linemod_detector/include/new_modalities.hpp
+++ ros_workspace/src/linemod_detector/include/new_modalities.hpp
@@ -3,6 +3,9 @@
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/core/core_c.h"
 #include "opencv2/core/internal.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
 
 namespace cv
 {
