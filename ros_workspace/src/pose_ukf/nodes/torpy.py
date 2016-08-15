3f9ba5b6e93a79f47c616844148d68f41918056b
contradict <contradict@gmail.com>
2016-08-14 20:14:33 -0700

    Emit rpy in degrees too

--- ros_workspace/src/pose_ukf/nodes/torpy.py
+++ ros_workspace/src/pose_ukf/nodes/torpy.py
@@ -3,16 +3,20 @@ import rospy
 from geometry_msgs.msg import PoseStamped, Vector3
 from sensor_msgs.msg import Imu
 import tf
+import numpy as np
 
 class ToRPY(object):
     def __init__(self):
         self.pub = rospy.Publisher('~rpy', Vector3, queue_size=1)
-        self.sub = rospy.Subscriber('pose', PoseStamped, self.resendPose)
-        self.sub = rospy.Subscriber('imu', Imu, self.resendImu)
+        self.pub_deg = rospy.Publisher('~rpy_deg', Vector3, queue_size=1)
+        self.sub_pose = rospy.Subscriber('pose', PoseStamped, self.resendPose)
+        self.sub_imu = rospy.Subscriber('imu', Imu, self.resendImu)
 
     def sendrpy(self, q):
         v = Vector3(*tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w]))
         self.pub.publish(v)
+        v_deg = Vector3(np.degrees(v.x), np.degrees(v.y), np.degrees(v.z))
+        self.pub_deg.publish(v_deg)
 
     def resendPose(self, msg):
         self.sendrpy(msg.pose.orientation)
